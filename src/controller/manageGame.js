10ce505a8bf0add254c0fdf9b59e991fc92ef624
MakinoharaShoko <salufthansa@foxmail.com>
2022-01-22 23:17:49 +0800

    添加创建游戏功能

--- src/controller/manageGame.js
+++ src/controller/manageGame.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const manageGame = express.Router();
 const getGameList = require("../service/getGameList")
+const createNewGame = require("../service/createNewGame");
 
 manageGame.get('/gameList', async (req, res) => {
     let gameList;
@@ -10,6 +11,20 @@ manageGame.get('/gameList', async (req, res) => {
 
 manageGame.get('/test', (req, res) => {
     res.send('ManageGame Test OK!');
+})
+
+manageGame.get('/createGame/*', async (req, res) => {
+    let url = req.url;
+    url = url.split('/');
+    let gameName = url[url.length - 1];
+    console.log(gameName);
+    let createResult;
+    createResult = await createNewGame(gameName);
+    if (createResult) {
+        res.send('OK')
+    } else {
+        res.send('AlreadyExist');
+    }
 })
 
 module.exports = manageGame;
