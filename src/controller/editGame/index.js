4458bbcf17c0bfdec2dc3853fc165c3410aa0493
MakinoharaShoko <salufthansa@foxmail.com>
2022-01-28 19:33:20 +0800

    初步建立创建新文件夹的逻辑

--- src/controller/editGame/index.js
+++ src/controller/editGame/index.js
@@ -1,14 +1,20 @@
 const express = require('express');
 const getAssets = require("./getAssets");
 const addAsset = require("./addAsset");
+const mkDir = require("../../service/mkDir");
 const editGame = express.Router();
 
-editGame.get('/test',(req, res)=>{
+editGame.get('/test', (req, res) => {
     res.send('Test Edit OK');
 })
 
-editGame.use('/getAssets',getAssets);
+editGame.use('/getAssets', getAssets);
 // 上传文件资源
 editGame.use('/addAsset', addAsset);
+
+editGame.post('/mkdir', async (req, res) => {
+    const result = await mkDir(req.body['current'], req.body['Name']);
+    res.send(result);
+})
 
 module.exports = editGame;
