9908e069171330d5444006b9dc22040303163026
MakinoharaShoko <salufthansa@foxmail.com>
2022-01-29 14:06:54 +0800

    建立新建场景的逻辑

--- src/controller/editGame/index.js
+++ src/controller/editGame/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const getAssets = require("./getAssets");
 const addAsset = require("./addAsset");
 const mkDir = require("../../service/mkDir");
+const addScene = require("../../service/addScene");
 const editGame = express.Router();
 
 editGame.get('/test', (req, res) => {
@@ -12,8 +13,16 @@ editGame.use('/getAssets', getAssets);
 // 上传文件资源
 editGame.use('/addAsset', addAsset);
 
+//创建新目录
 editGame.post('/mkdir', async (req, res) => {
     const result = await mkDir(req.body['current'], req.body['Name']);
+    res.send(result);
+})
+
+editGame.post('/addNewScene', async (req, res) => {
+    const gameName = req.body['gameName'];
+    const sceneName = req.body['sceneName'];
+    const result = await addScene(gameName, sceneName);
     res.send(result);
 })
 
