5f9f5bf31f2f1ec65115e71cdf0f3c9845a3e352
Mahiru <makinoharashoko@outlook.com>
2022-03-11 20:13:46 +0800

    更新模板文件
    Former-commit-id: faf095d2b281a9f71df3dbdccac06f040e800dee

--- src/extend/logger.js
+++ src/extend/logger.js
@@ -1,4 +1,4 @@
 const logger = new (require('cloudlogjs'))();
-logger.init('https://log.msfasr.com');
-logger.setCollection('WebGAL_Origine')
+// logger.init('https://log.msfasr.com');
+// logger.setCollection('WebGAL_Origine')
 module.exports = logger;
