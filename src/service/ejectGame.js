89f9c1315336bf2cc384ed42f6135fec6ae05d9a
Mahiru <makinoharashoko@outlook.com>
2022-05-18 16:25:19 +0800

    feat: try to delete useless file

--- src/service/ejectGame.js
+++ src/service/ejectGame.js
@@ -10,7 +10,7 @@ const {webgalMkdir, webgalCopy} = require("../util/webgal-fs");
  */
 ejectGame = async (gameName, ejectPlatform) => {
     // 根据 GameName 找到游戏所在目录
-    const gameDir = `./public/Games/${gameName}/`;
+    const gameDir = `./public/Games/${gameName}/game/`;
     // 创建游戏导出目录
     const exportDir = `./Exported_Games/${gameName}`;
     await webgalMkdir(exportDir);
@@ -19,12 +19,14 @@ ejectGame = async (gameName, ejectPlatform) => {
         const electronExportDir = `${exportDir}/electron-windows`;
         await webgalMkdir(electronExportDir);
         await webgalCopy(`./WebGAL_Electron_Template/`, `${electronExportDir}/`);
-        await webgalCopy(gameDir, `${electronExportDir}/resources/app/public/`);
+        await webgalCopy(gameDir, `${electronExportDir}/resources/app/public/game/`);
+        await webgalCopy('./WebGAL_Template/', `${electronExportDir}/resources/app/public/`);
     }
     if (ejectPlatform === 'web') {
         const webExportDir = `${exportDir}/web`;
         await webgalMkdir(webExportDir);
-        await webgalCopy(gameDir, `${webExportDir}/`);
+        await webgalCopy(gameDir, `${webExportDir}/game/`);
+        await webgalCopy('./WebGAL_Template/', `${webExportDir}/`);
     }
 }
 
