91e8336c4116b9346b9ebb8667477841b42b6c17
Mahiru <Mahiru_@outlook.com>
2022-07-02 13:27:44 +0800

    delete terre1 files from new branch
    Former-commit-id: 28ae09e9e735ace77d309ec0c144bfba94d1292f

--- packages/Origine/src/util/sendWsSync.js
+++
@@ -1,9 +0,0 @@
-import runtime from "../env/runtime";
-
-export const sendWsSync = (id) => {
-    const ws = runtime.wsConn;
-    if (runtime.isRealtimeRefreashPreview)
-        setTimeout(() => {
-            ws.send(`jmp ${runtime.currentEditScene} ${id+1}`);
-        }, 500);
-}
