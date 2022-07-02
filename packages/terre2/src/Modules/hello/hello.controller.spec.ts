60e62388bfd7ac4083c32d43a04bb7f95dc21e51
Mahiru <Mahiru_@outlook.com>
2022-07-02 13:24:09 +0800

    init terre 2

---
+++ packages/terre2/src/Modules/hello/hello.controller.spec.ts
@@ -0,0 +1,20 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { HelloController } from './hello.controller';
+import { HelloService } from './hello.service';
+
+describe('HelloController', () => {
+  let controller: HelloController;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [HelloController],
+      providers: [HelloService],
+    }).compile();
+
+    controller = module.get<HelloController>(HelloController);
+  });
+
+  it('should be defined', () => {
+    expect(controller).toBeDefined();
+  });
+});
