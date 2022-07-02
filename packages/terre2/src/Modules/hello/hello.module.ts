dba0a0c92731aae5aa01488302f0d072e26f2833
Mahiru <Mahiru_@outlook.com>
2022-07-02 13:25:05 +0800

    remove terre2 files from brunch 1.x

--- packages/terre2/src/Modules/hello/hello.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { HelloService } from './hello.service';
-import { HelloController } from './hello.controller';
-
-@Module({
-  providers: [HelloService],
-  controllers: [HelloController],
-})
-export class HelloModule {}
--- packages/terre2/src/app.service.ts
+++
@@ -1,8 +0,0 @@
-import { Injectable } from '@nestjs/common';
-
-@Injectable()
-export class AppService {
-  getApiTest() {
-    return 'API Test OK';
-  }
-}
