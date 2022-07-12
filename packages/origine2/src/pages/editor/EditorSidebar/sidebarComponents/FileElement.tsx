d5465bcc04ccd14b53f36346831067365cecac4e
Mahiru <Mahiru_@outlook.com>
2022-07-12 23:08:49 +0800

    feat: scene name editing function.
    Former-commit-id: 016ecee1a23180b96aa26a1e8fdc0dba6ca0e70c

--- packages/origine2/src/pages/editor/EditorSidebar/sidebarComponents/FileElement.tsx
+++ packages/origine2/src/pages/editor/EditorSidebar/sidebarComponents/FileElement.tsx
@@ -1,12 +1,83 @@
-export interface IFileElementProps{
-  name:string
-  fileType?:string;
-  editFileNameCallback?:Function;
-  clickCallback?:Function;
-  deleteCallback?:Function;
+import { DeleteOne, Editor, Notes } from "@icon-park/react";
+import React, { MouseEventHandler, ReactElement } from "react";
+import styles from "./sidebarComponents.module.scss";
+import { useValue } from "../../../../hooks/useValue";
+import { Callout, Text, Link, TextField, PrimaryButton } from "@fluentui/react";
+import { useId } from "@fluentui/react-hooks";
+
+export interface IFileElementProps {
+  name: string;
+  fileType?: string;
+  editFileNameCallback?: Function;
+  clickCallback?: Function;
+  deleteCallback?: Function;
+  icon?: ReactElement;
 }
 
 
-export default function FileElement(){
+export default function FileElement(props: IFileElementProps) {
+  const icon = props.icon ?? <Notes theme="outline" size="24" fill="#333" strokeWidth={3} />;
 
+  // 修改文件名部分
+  const showEditNameCallout = useValue(false);
+  const newFileName = useValue<string>(props.name);
+
+  function closeEditNameCallout() {
+    showEditNameCallout.set(false);
+  }
+
+  function switchEditNameCallout() {
+    showEditNameCallout.set(!showEditNameCallout.value);
+  }
+
+  function updateNewFilename(event: any) {
+    const newValue = event.target.value;
+    newFileName.set(newValue);
+  }
+
+  function commitNewFileName() {
+    if (props.editFileNameCallback)
+      props.editFileNameCallback(newFileName.value);
+  }
+
+  const clickCallback = () => {
+    console.log("clicked");
+    props?.clickCallback && props.clickCallback();
+  };
+
+  const buttonId = useId(`button`);
+
+  // @ts-ignore
+  // @ts-ignore
+  return <div className={styles.fileElement}>
+    <div className={styles.fileElement_icon} onClick={clickCallback}>{icon}</div>
+    <div className={styles.fileElement_name} onClick={clickCallback}>{props.name}</div>
+    <div id={`current_${props.name}`} className={styles.fileElement_interactable_icon} onClick={switchEditNameCallout}>
+      <Editor id={buttonId} theme="outline" size="24" fill="#333" strokeWidth={3} />
+      {showEditNameCallout.value && <Callout
+        className={styles.callout}
+        ariaLabelledBy="editName"
+        ariaDescribedBy="editName"
+        role="dialog"
+        gapSpace={0}
+        target={`#${buttonId}`}
+        onDismiss={closeEditNameCallout}
+        setInitialFocus
+        style={{ width: "300px", padding: "5px 10px 5px 10px" }}
+      >
+        <Text block variant="xLarge" className={styles.title} id="editNameTitle">
+          修改文件名
+        </Text>
+        <div>
+          <TextField defaultValue={newFileName.value} onChange={updateNewFilename} label="新文件名" />
+        </div>
+        <div style={{ display: "flex", justifyContent: "center", padding: "5px 0 5px 0" }}>
+          <PrimaryButton text="修改" onClick={commitNewFileName} allowDisabledFocus />
+        </div>
+      </Callout>}
+    </div>
+    <div className={styles.fileElement_interactable_icon} onClick={props.deleteCallback as MouseEventHandler}>
+      <DeleteOne theme="outline" size="24" fill="#333" strokeWidth={3} />
+    </div>
+  </div>;
 }
