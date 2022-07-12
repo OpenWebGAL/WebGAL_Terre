103e7c429129955dd487a97186c1615b256b2b46
Mahiru <Mahiru_@outlook.com>
2022-07-12 23:08:49 +0800

    feat: scene name editing function.
    Former-commit-id: 5cc6e0b11e318eb035445ca8605c41ab73da1ee7

--- packages/origine2/src/pages/editor/EditorSidebar/SidebarTags/Scenes.tsx
+++ packages/origine2/src/pages/editor/EditorSidebar/SidebarTags/Scenes.tsx
@@ -4,29 +4,54 @@ import { useEffect } from "react";
 import { useSelector } from "react-redux";
 import { RootState } from "../../../../store/origineStore";
 import axios from "axios";
+import { IFileInfo } from "webgal-terre-2/dist/Modules/webgal-fs/webgal-fs.service";
+import FileElement from "../sidebarComponents/FileElement";
 
 export default function Scenes() {
   const state = useSelector((state: RootState) => state.status.editor);
   const currentGameName = state.currentEditingGame;
   // 场景文件的列表
-  const sceneList = useValue([]);
+  const sceneList = useValue<IFileInfo[]>([]);
 
   // 请求场景文件的函数
   async function getSceneList() {
     const url = `/api/manageGame/readGameAssets/${currentGameName}/game/scene`;
-    const rawSceneList = await axios.get(url).then(r => r.data.dirInfo);
+    const rawSceneList: IFileInfo[] = await axios.get(url).then(r => r.data.dirInfo);
     return rawSceneList.filter((e: any) => e.extName === ".txt");
+  }
+
+  function updateSceneListView() {
+    getSceneList().then(result => {
+      sceneList.set(result);
+    });
   }
 
   // 准备请求场景文件
   useEffect(() => {
-    getSceneList().then(result => {
-      sceneList.set(result);
-      console.log(result);
-    });
+    updateSceneListView();
   }, []);
+
+  // 更新文件名的函数
+  function constructUpdateFilenameFunc(oldPath: string) {
+    return function(newFilename: string) {
+      const params = new URLSearchParams();
+      params.append("path", oldPath);
+      params.append("newName", newFilename);
+      axios.post("/api/manageGame/editFilename/", params).then(
+        updateSceneListView
+      );
+    };
+  }
+
+  const showSceneList = sceneList.value.map(singleFile => {
+    return <FileElement name={singleFile.name} key={singleFile.name}
+      editFileNameCallback={constructUpdateFilenameFunc(singleFile.path)} />;
+  });
 
   return <div>
     <div className={styles.sidebar_tag_title}>场景管理</div>
+    <div>
+      {showSceneList}
+    </div>
   </div>;
 }
