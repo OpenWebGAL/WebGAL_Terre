fb0aa2a8b138a8ed9e54f7e6fbdf15cdda23ff6e
Mahiru <Mahiru_@outlook.com>
2022-08-09 22:39:26 +0800

    feat: add edit config feature.

--- packages/origine2/src/pages/editor/EditorSidebar/SidebarTags/Assets.tsx
+++ packages/origine2/src/pages/editor/EditorSidebar/SidebarTags/Assets.tsx
@@ -1,7 +1,9 @@
 import styles from './sidebarTags.module.scss';
 
-export default function Assets(){
-  return <div>
-    <div className={styles.sidebar_tag_title}>资源管理</div>
-  </div>;
+export default function Assets() {
+  return (
+    <div>
+      <div className={styles.sidebar_tag_title}>资源管理</div>
+    </div>
+  );
 }
