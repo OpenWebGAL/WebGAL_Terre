605c2cee8d2fb851f3afced7c12124948e7c0cc9
Mahiru <Mahiru_@outlook.com>
2022-08-09 22:39:26 +0800

    feat: add edit config feature.
    Former-commit-id: fb0aa2a8b138a8ed9e54f7e6fbdf15cdda23ff6e

--- packages/origine2/src/pages/editor/EditorSidebar/SidebarTags/GameConfig.tsx
+++ packages/origine2/src/pages/editor/EditorSidebar/SidebarTags/GameConfig.tsx
@@ -3,7 +3,9 @@ import { useValue } from "../../../../hooks/useValue";
 import axios from "axios";
 import { useSelector } from "react-redux";
 import { RootState } from "../../../../store/origineStore";
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
+import { cloneDeep } from "lodash";
+import { ITextField, TextField } from "@fluentui/react";
 
 interface IGameConfig {
   gameName: string;
@@ -12,13 +14,14 @@ interface IGameConfig {
 }
 
 export default function GameConfig() {
-
   const state = useSelector((state: RootState) => state.status.editor);
 
   // 拿到游戏配置
   const gameConfig = useValue<IGameConfig>({ gameName: "", titleBgm: "", titleBackground: "" });
   const getGameConfig = () => {
-    axios.get(`/api/manageGame/getGameConfig/${state.currentEditingGame}`).then(r => parseAndSetGameConfigState(r.data));
+    axios
+      .get(`/api/manageGame/getGameConfig/${state.currentEditingGame}`)
+      .then((r) => parseAndSetGameConfigState(r.data));
   };
 
   function parseAndSetGameConfigState(data: string) {
@@ -28,12 +31,12 @@ export default function GameConfig() {
     const dataArray: string[] = newData.split("\n");
     // 对于每一行，，截取分号，找出键值
     let dataWithKeyValue = dataArray.map((e: string) => {
-      let commandText = e.replaceAll(/[;；]/g,'');
+      let commandText = e.replaceAll(/[;；]/g, "");
       return commandText.split(":");
     });
-    dataWithKeyValue = dataWithKeyValue.filter(e => e.length >= 2);
+    dataWithKeyValue = dataWithKeyValue.filter((e) => e.length >= 2);
     // 开始修改
-    dataWithKeyValue.forEach(e => {
+    dataWithKeyValue.forEach((e) => {
       switch (e[0]) {
       case "Game_name":
         gameConfig.set({ ...gameConfig.value, gameName: e[1] });
@@ -45,7 +48,7 @@ export default function GameConfig() {
         gameConfig.set({ ...gameConfig.value, titleBackground: e[1] });
         break;
       default:
-        console.log('NOT PARSED');
+        console.log("NOT PARSED");
       }
     });
   }
@@ -54,18 +57,59 @@ export default function GameConfig() {
     getGameConfig();
   }, []);
 
-  return <div>
-    <div className={styles.sidebar_tag_title}>游戏配置</div>
+  function updateGameConfig(key: keyof IGameConfig, content: string) {
+    const draft = cloneDeep(gameConfig.value);
+    draft[key] = content;
+    gameConfig.set(draft);
+    const newConfig = `Game_name:${gameConfig.value.gameName};\nTitle_bgm:${gameConfig.value.titleBgm};\nTitle_img:${gameConfig.value.titleBackground};\n`;
+    const form = new URLSearchParams();
+    form.append('gameName',state.currentEditingGame);
+    form.append('newConfig',newConfig);
+    axios.post(`/api/manageGame/setGameConfig/`,form).then(getGameConfig);
+  }
+
+
+  return (
     <div>
+      <div className={styles.sidebar_tag_title}>游戏配置</div>
       <div>
-        游戏名称：{gameConfig.value.gameName}
-      </div>
-      <div>
-        标题背景图片：{gameConfig.value.titleBackground}
-      </div>
-      <div>
-        标题背景音乐：{gameConfig.value.titleBgm}
+        <div className={styles.sidebar_gameconfig_container}>
+          <div className={styles.sidebar_gameconfig_title}>游戏名称</div>
+          <GameConfigEditor key="gameName" value={gameConfig.value.gameName} onChange={(e:string)=>updateGameConfig("gameName", e)}/>
+        </div>
+        <div className={styles.sidebar_gameconfig_container}>
+          <div className={styles.sidebar_gameconfig_title}>标题背景图片</div>
+          <GameConfigEditor key="gameName" value={gameConfig.value.titleBackground} onChange={(e:string)=>updateGameConfig("titleBackground", e)}/>
+        </div>
+        <div className={styles.sidebar_gameconfig_container}>
+          <div className={styles.sidebar_gameconfig_title}>标题背景音乐</div>
+          <GameConfigEditor key="gameName" value={gameConfig.value.titleBgm} onChange={(e:string)=>updateGameConfig("titleBgm", e)}/>
+        </div>
       </div>
     </div>
+  );
+}
+
+interface IGameConfigEditor {
+  key: keyof IGameConfig;
+  value: string;
+  onChange: Function;
+}
+
+function GameConfigEditor(props: IGameConfigEditor) {
+  const showEditBox = useValue(false);
+  const currentValue= useValue(props.value);
+  const inputBoxRef = useRef<ITextField>(null);
+  return <div>
+    {!showEditBox.value && props.value}
+    {!showEditBox.value && <div className={styles.editButton} onClick={()=>{showEditBox.set(true);
+      setTimeout(()=>inputBoxRef.current?.focus(),100);
+    }}>修改</div>}
+    {showEditBox.value && <TextField componentRef={inputBoxRef} value={currentValue.value}
+      onChange={(event, newValue) => {
+        currentValue.set(newValue);
+      }}
+      onBlur={()=>{props.onChange(currentValue.value);showEditBox.set(false);}}
+    />}
   </div>;
 }
--- packages/origine2/src/pages/editor/EditorSidebar/SidebarTags/Scenes.tsx
+++ packages/origine2/src/pages/editor/EditorSidebar/SidebarTags/Scenes.tsx
@@ -1,13 +1,13 @@
-import styles from "./sidebarTags.module.scss";
-import { useValue } from "../../../../hooks/useValue";
-import React, { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { RootState } from "../../../../store/origineStore";
-import axios from "axios";
-import { IFileInfo } from "webgal-terre-2/dist/Modules/webgal-fs/webgal-fs.service";
-import FileElement from "../sidebarComponents/FileElement";
-import { Callout, PrimaryButton, Text, TextField } from "@fluentui/react";
-import { ITag, statusActions } from "../../../../store/statusReducer";
+import styles from './sidebarTags.module.scss';
+import { useValue } from '../../../../hooks/useValue';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { RootState } from '../../../../store/origineStore';
+import axios from 'axios';
+import { IFileInfo } from 'webgal-terre-2/dist/Modules/webgal-fs/webgal-fs.service';
+import FileElement from '../sidebarComponents/FileElement';
+import { Callout, PrimaryButton, Text, TextField } from '@fluentui/react';
+import { ITag, statusActions } from '../../../../store/statusReducer';
 
 export default function Scenes() {
   const state = useSelector((state: RootState) => state.status.editor);
@@ -18,7 +18,7 @@ export default function Scenes() {
 
   // 处理新建场景的问题
   const showCreateSceneCallout = useValue(false);
-  const newSceneName = useValue("");
+  const newSceneName = useValue('');
   const updateNewSceneName = (event: any) => {
     const newValue = event.target.value;
     newSceneName.set(newValue);
@@ -26,25 +26,24 @@ export default function Scenes() {
   const createNewScene = () => {
     const gameName = state.currentEditingGame;
     const params = new URLSearchParams();
-    params.append("gameName", gameName);
-    params.append("sceneName", newSceneName.value);
-    axios.post("/api/manageGame/createNewScene/", params).then(() => {
+    params.append('gameName', gameName);
+    params.append('sceneName', newSceneName.value);
+    axios.post('/api/manageGame/createNewScene/', params).then(() => {
       showCreateSceneCallout.set(false);
       updateSceneListView();
-      newSceneName.set("");
+      newSceneName.set('');
     });
   };
-
 
   // 请求场景文件的函数
   async function getSceneList() {
     const url = `/api/manageGame/readGameAssets/${currentGameName}/game/scene`;
-    const rawSceneList: IFileInfo[] = await axios.get(url).then(r => r.data.dirInfo);
-    return rawSceneList.filter((e: any) => e.extName === ".txt");
+    const rawSceneList: IFileInfo[] = await axios.get(url).then((r) => r.data.dirInfo);
+    return rawSceneList.filter((e: any) => e.extName === '.txt');
   }
 
   function updateSceneListView() {
-    getSceneList().then(result => {
+    getSceneList().then((result) => {
       sceneList.set(result);
     });
   }
@@ -56,81 +55,82 @@ export default function Scenes() {
 
   // 更新文件名的函数
   function constructUpdateFilenameFunc(oldPath: string) {
-    return function(newFilename: string) {
+    return function (newFilename: string) {
       const params = new URLSearchParams();
-      params.append("path", oldPath);
-      params.append("newName", newFilename);
-      axios.post("/api/manageGame/editFilename/", params).then(
-        updateSceneListView
-      );
+      params.append('path', oldPath);
+      params.append('newName', newFilename);
+      axios.post('/api/manageGame/editFilename/', params).then(updateSceneListView);
     };
   }
 
   // 删除文件的函数
   function constructDeleteFileFunc(path: string) {
-    return function() {
+    return function () {
       const params = new URLSearchParams();
-      params.append("path", path);
-      axios.post("/api/manageGame/deleteFile/", params).then(
-        updateSceneListView
-      );
+      params.append('path', path);
+      axios.post('/api/manageGame/deleteFile/', params).then(updateSceneListView);
     };
   }
 
   // 添加 Tag 的函数
   function addEditTag(name: string, target: string) {
-    const tag: ITag = { tagName: name, tagTarget: target, tagType: "scene" };
+    const tag: ITag = { tagName: name, tagTarget: target, tagType: 'scene' };
     // 先要确定没有这个tag
     const result = state.tags.findIndex((e) => e.tagTarget === target);
-    if (result < 0)
-      dispatch(statusActions.addEditAreaTag(tag));
+    if (result < 0) dispatch(statusActions.addEditAreaTag(tag));
     dispatch(statusActions.setCurrentTagTarget(target));
   }
 
-  const showSceneList = sceneList.value.map(singleFile => {
-    return <FileElement
-      clickCallback={() => addEditTag(singleFile.name, singleFile.path)}
-      name={singleFile.name}
-      key={singleFile.name}
-      deleteCallback={constructDeleteFileFunc(singleFile.path)}
-      editFileNameCallback={constructUpdateFilenameFunc(singleFile.path)} />;
+  const showSceneList = sceneList.value.map((singleFile) => {
+    return (
+      <FileElement
+        clickCallback={() => addEditTag(singleFile.name, singleFile.path)}
+        name={singleFile.name}
+        key={singleFile.name}
+        deleteCallback={constructDeleteFileFunc(singleFile.path)}
+        editFileNameCallback={constructUpdateFilenameFunc(singleFile.path)}
+      />
+    );
   });
 
-  return <div>
-    <div className={styles.sidebar_tag_head}>
-      <div className={styles.sidebar_tag_title}>
-        场景管理
-      </div>
-      <div className={styles.sidebar_tag_head_button}>
-        <PrimaryButton id="createSceneButton" text="新建场景"
-          onClick={() => showCreateSceneCallout.set(!showCreateSceneCallout.value)} />
-        {showCreateSceneCallout.value && <Callout
-          className={styles.callout}
-          ariaLabelledBy="createNewSceneCallout"
-          ariaDescribedBy="createNewSceneCallout"
-          role="dialog"
-          gapSpace={0}
-          target="#createSceneButton"
-          onDismiss={() => {
-            showCreateSceneCallout.set(false);
-          }}
-          setInitialFocus
-          style={{ width: "300px", padding: "5px 10px 5px 10px" }}
-        >
-          <Text block variant="xLarge" className={styles.title}>
-            创建新场景
-          </Text>
-          <div>
-            <TextField defaultValue={newSceneName.value} onChange={updateNewSceneName} label="新场景名" />
-          </div>
-          <div style={{ display: "flex", justifyContent: "center", padding: "5px 0 5px 0" }}>
-            <PrimaryButton text="创建" onClick={createNewScene} allowDisabledFocus />
-          </div>
-        </Callout>}
-      </div>
-    </div>
+  return (
     <div>
-      {showSceneList}
+      <div className={styles.sidebar_tag_head}>
+        <div className={styles.sidebar_tag_title}>场景管理</div>
+        <div className={styles.sidebar_tag_head_button}>
+          <PrimaryButton
+            id="createSceneButton"
+            text="新建场景"
+            onClick={() => showCreateSceneCallout.set(!showCreateSceneCallout.value)}
+          />
+          {showCreateSceneCallout.value && (
+            <Callout
+              className={styles.callout}
+              ariaLabelledBy="createNewSceneCallout"
+              ariaDescribedBy="createNewSceneCallout"
+              role="dialog"
+              gapSpace={0}
+              target="#createSceneButton"
+              onDismiss={() => {
+                showCreateSceneCallout.set(false);
+              }}
+              setInitialFocus
+              style={{ width: '300px', padding: '5px 10px 5px 10px' }}
+            >
+              <Text block variant="xLarge" className={styles.title}>
+                创建新场景
+              </Text>
+              <div>
+                <TextField defaultValue={newSceneName.value} onChange={updateNewSceneName} label="新场景名" />
+              </div>
+              <div style={{ display: 'flex', justifyContent: 'center', padding: '5px 0 5px 0' }}>
+                <PrimaryButton text="创建" onClick={createNewScene} allowDisabledFocus />
+              </div>
+            </Callout>
+          )}
+        </div>
+      </div>
+      <div>{showSceneList}</div>
     </div>
-  </div>;
+  );
 }
