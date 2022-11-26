431752cee775240ef1777d00e4c4553126edbf8e
Mahiru <Mahiru_@outlook.com>
2022-11-26 12:19:07 +0800

    fix: 文本编辑器标记中文字符

--- packages/origine2/src/pages/editor/Topbar/Topbar.tsx
+++ packages/origine2/src/pages/editor/Topbar/Topbar.tsx
@@ -1,11 +1,16 @@
 import styles from "./topbar.module.scss";
 import { useSelector } from "react-redux";
 import { RootState } from "../../../store/origineStore";
+import {  LeftSmall } from "@icon-park/react";
 
 export default function TopBar() {
   const editingGame: string = useSelector((state: RootState) => state.status.editor.currentEditingGame);
   return <div className={styles.editor_topbar}>
-    <div className={styles.editor_title}>WebGAL Origine</div>
+    <a href="/" className={styles.home_btn}>
+      <LeftSmall theme="outline" size="24" fill="#005caf"/>
+      <div className={styles.editor_title}>WebGAL Origine</div>
+    </a>
+
     <div className={styles.editor_editingGame}>正在编辑：<span style={{fontWeight:"bold"}}>{editingGame}</span></div>
   </div>;
 }
