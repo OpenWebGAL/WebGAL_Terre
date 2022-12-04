edd2a8ab03fc50d12e16368d2c56b322d57a7b45
Mahiru <Mahiru_@outlook.com>
2022-12-04 18:25:08 +0800

    feat: 游戏导出

--- packages/origine2/src/main.tsx
+++ packages/origine2/src/main.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import { initializeIcons } from '@fluentui/font-icons-mdl2';
+initializeIcons();
 
 // 不用 StrictMode，因为会和 react-butiful-dnd 冲突
 ReactDOM.createRoot(document.getElementById("root")!).render(
--- packages/origine2/src/pages/dashboard/Sidebar.tsx
+++ packages/origine2/src/pages/dashboard/Sidebar.tsx
@@ -1,5 +1,7 @@
 import GameElement from "./GameElement";
 import styles from "./sidebar.module.scss";
+import React, { useRef, useState } from "react";
+import { Callout, PrimaryButton, Text, TextField } from "@fluentui/react";
 
 interface ISidebarProps {
   gameList: string[];
@@ -10,17 +12,53 @@ interface ISidebarProps {
 
 
 export default function Sidebar(props: ISidebarProps) {
+
+  const [showCreateGameCallout, setShowCreateGameCallout] = useState(false);
+  const [newGameName, setNewGameName] = useState("新的游戏");
+
   const showGameList = props.gameList.map(e => {
     const checked = props.currentSetGame === e;
     return <GameElement onClick={() => props.setCurrentGame(e)} gameName={e} key={e} checked={checked} />;
   });
+
+  function createNewGame() {
+    props.createGame(newGameName);
+    setShowCreateGameCallout(false);
+  }
 
 
   return <div className={styles.sidebar_main}>
     <div className={styles.sidebar_top}>
       <span className={styles.sidebar_top_title}>游戏列表</span>
       { /* @ts-ignore} */}
-      <span onClick={props.createGame} className={styles.createGameButton}>新建游戏</span>
+      <span id="new-game-button" onClick={() => {
+        setShowCreateGameCallout(!showCreateGameCallout);
+      }} className={styles.createGameButton}>新建游戏</span>
+      {showCreateGameCallout && <Callout
+        className={styles.callout}
+        ariaLabelledBy="createNewSceneCallout"
+        ariaDescribedBy="createNewSceneCallout"
+        role="dialog"
+        gapSpace={0}
+        target="#new-game-button"
+        onDismiss={() => {
+          setShowCreateGameCallout(false);
+        }}
+        setInitialFocus
+        style={{ width: "300px", padding: "5px 10px 5px 10px" }}
+      >
+        <Text block variant="xLarge" className={styles.title}>
+          创建新游戏
+        </Text>
+        <div>
+          <TextField onChange={(event, newValue) => {
+            setNewGameName(newValue??'');
+          }} defaultValue="新的游戏" label="新游戏名" />
+        </div>
+        <div style={{ display: "flex", justifyContent: "center", padding: "5px 0 5px 0" }}>
+          <PrimaryButton text="创建" onClick={createNewGame} allowDisabledFocus />
+        </div>
+      </Callout>}
     </div>
     {showGameList}
   </div>;
