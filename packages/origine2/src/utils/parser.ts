66c17beb3be803cfdf45243cb2732e1fbf9d9e84
Mahiru <Mahiru_@outlook.com>
2022-12-30 19:17:09 +0800

    upgrade webgal parser
    Former-commit-id: ab9102ca281c0cd238433adb389d00f37230cfd8

---
+++ packages/origine2/src/utils/parser.ts
@@ -0,0 +1,22 @@
+import SceneParser from "webgal-parser";
+import { IScene } from "webgal-parser/interface/sceneInterface";
+import { logger } from "./logger";
+import { ADD_NEXT_ARG_LIST, SCRIPT_CONFIG } from "webgal-parser/config/scriptConfig";
+
+const parser = new SceneParser((assetList) => {
+}, (fileName, assetType) => {
+  return fileName;
+}, ADD_NEXT_ARG_LIST, SCRIPT_CONFIG);
+
+/**
+ * 场景解析器
+ * @param rawScene 原始场景
+ * @param sceneName 场景名称
+ * @param sceneUrl 场景url
+ * @return {IScene} 解析后的场景
+ */
+export const sceneParser = (rawScene: string, sceneName: string, sceneUrl: string): IScene => {
+  const parsedScene = parser.parse(rawScene, sceneName, sceneUrl);
+  logger.info(`解析场景：${sceneName}，数据为：`, parsedScene);
+  return parsedScene;
+};
