eec960342d02fc841347a0836e81fd77af7ad71b
Mahiru <Mahiru_@outlook.com>
2023-01-13 13:28:31 +0800

    feat: 游戏配置选项卡的文件选择

--- packages/origine2/src/pages/editor/ChooseFile/ChooseFile.tsx
+++ packages/origine2/src/pages/editor/ChooseFile/ChooseFile.tsx
@@ -1,32 +1,36 @@
 import { useValue } from "../../../hooks/useValue";
 import { useEffect } from "react";
 import axios from "axios";
-import { IFileInfo } from "webgal-terre-2/dist/Modules/webgal-fs/webgal-fs.service";
 import { useSelector } from "react-redux";
 import { RootState } from "../../../store/origineStore";
-import { logger } from "../../../utils/logger";
 import { useId } from "@fluentui/react-hooks";
-import { Callout, DefaultButton, Link, Text } from "@fluentui/react";
-import styles from './chooseFile.module.scss';
+import { Callout, DefaultButton, Text } from "@fluentui/react";
+import styles from "./chooseFile.module.scss";
 
 export interface IChooseFile {
   sourceBase: string;
-  onChange: (choosedFileName: string) => void;
+  onChange: (choosedFileDescription:IFileDescription | null) => void;
   // 拓展名，要加.
   extName: string[];
+}
+
+export interface IFileDescription {
+  extName: string;
+  isDir: boolean;
+  name: string;
+  path: string;
 }
 
 export default function ChooseFile(props: IChooseFile) {
   const currentChildDir = useValue([]);
   const currentDirName = props.sourceBase + currentChildDir.value.reduce((prev, curr) => prev + "/" + curr, "");
-  const currentDirFiles = useValue([]);
+  const currentDirFiles = useValue<IFileDescription[]>([]);
   const gameName = useSelector((state: RootState) => state.status.editor.currentEditingGame);
   useEffect(() => {
     /**
      * 更新当前目录内的文件
      */
     getFileList(gameName, currentDirName, props.extName).then(result => {
-      logger.debug("当前目录文件", result);
       currentDirFiles.set(result);
     });
 
@@ -34,23 +38,32 @@ export default function ChooseFile(props: IChooseFile) {
 
   const isShowChooseFileCallout = useValue(false);
   const buttonId = useId("choosefile-callout");
-  const labelId = useId("callout-label");
-  const descriptionId = useId("callout-description");
 
   function toggleIsCalloutVisible() {
     isShowChooseFileCallout.set(!isShowChooseFileCallout.value);
   }
 
+  function onChooseFile(fileDescription:IFileDescription){
+    toggleIsCalloutVisible();
+    props.onChange(fileDescription);
+  }
+
+  const fileSelectButtonList = currentDirFiles.value.map(file=>{
+    return <div key={file.path} className={styles.choseFileButton} onClick={()=>onChooseFile(file)}>
+      {file.name}
+    </div>;
+  });
+
   return <>
     <DefaultButton
       id={buttonId}
-      onClick={toggleIsCalloutVisible}
-      text={isShowChooseFileCallout.value ? "Hide callout" : "Show callout"}
+      onClick={isShowChooseFileCallout.value ? ()=>{toggleIsCalloutVisible();
+        props.onChange(null);
+      } : toggleIsCalloutVisible}
+      text={isShowChooseFileCallout.value ? "取消选择" : "选择文件"}
     />
     {isShowChooseFileCallout.value && (
       <Callout
-        ariaLabelledBy={labelId}
-        ariaDescribedBy={descriptionId}
         role="dialog"
         gapSpace={0}
         target={`#${buttonId}`}
@@ -58,9 +71,10 @@ export default function ChooseFile(props: IChooseFile) {
         setInitialFocus
         className={styles.callout}
       >
-        <Text as="h1" block variant="xLarge" id={labelId}>
+        <div className={styles.chooseFileCalloutTitle}>
           选择文件
-        </Text>
+        </div>
+        {fileSelectButtonList}
       </Callout>
     )}
   </>;
@@ -74,7 +88,7 @@ export default function ChooseFile(props: IChooseFile) {
  */
 async function getFileList(currentGameName: string, childDir: string, extName: string[]) {
   const url = `/api/manageGame/readGameAssets/${currentGameName}/game/${childDir}`;
-  const rawFileList: IFileInfo[] = await axios.get(url).then((r) => r.data.dirInfo);
+  const rawFileList: IFileDescription[] = await axios.get(url).then((r) => r.data.dirInfo);
   if (extName.length === 0) {
     return rawFileList;
   }
