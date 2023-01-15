b3939cd1a0fca87111a1371f67c32bc0fb19d0d7
Mahiru <Mahiru_@outlook.com>
2023-01-15 13:01:06 +0800

    chore: 优化调试体验，使用 node 反代，从而避免手动配置 nginx；
    Former-commit-id: 61e2fa3c2a4c4411ee9a378714e19fcd4f49b827

--- packages/terre2/src/main.ts
+++ packages/terre2/src/main.ts
@@ -1,27 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as ws from 'ws';
 import open = require('open');
-
-const wsServer = new ws.WebSocketServer({ port: 9999 });
-
-const connectionList = [];
-
-wsServer.on('connection', (conn) => {
-  connectionList.push(conn);
-  conn.on('message', (data) => {
-    const str = data.toString();
-    connectionList.forEach((e) => {
-      e.send(str);
-    });
-  });
-});
+import * as process from 'process';
 
 async function bootstrap() {
-  console.log(`WebGAL Terre 2.3.11 starting at ${process.cwd()}`);
   const app = await NestFactory.create(AppModule);
   await app.listen(3001);
-  open('http://localhost:3001');
 }
 
-bootstrap();
+bootstrap().then(() => {
+  console.log(`WebGAL Terre 2.3.11 starting at ${process.cwd()}`);
+  open('http://localhost:3001');
+});
