a6eafe0576769b7b25cffdb86affd1f48eb03f54
Mahiru <Mahiru_@outlook.com>
2023-02-21 21:36:02 +0800

    feat: add type check.
    Former-commit-id: e5efa98eaa44132f7651fec50be4e6102dc8d60d

--- packages/origine2/src/pages/editor/GraphicalEditor/SentenceEditor/index.tsx
+++ packages/origine2/src/pages/editor/GraphicalEditor/SentenceEditor/index.tsx
@@ -1,22 +1,30 @@
 import { commandType, ISentence } from "webgal-parser/src/interface/sceneInterface";
 import Say from "./Say";
+import { FC } from "react";
 
 export interface ISentenceEditorProps {
   sentence: ISentence;
   onSubmit: (newSentence: string) => void;
 }
 
-export const sentenceEditorDefault = {
+interface ISentenceEditorConfig {
+  type: commandType,
+  title: string,
+  initialText: string,
+  component: FC<ISentenceEditorProps>
+}
+
+export const sentenceEditorDefault: ISentenceEditorConfig = {
   type: commandType.say,
-  title:'普通对话',
+  title: "普通对话",
   initialText: "角色:对话;",
   component: Say
 };
 
-export const sentenceEditorConfig = [
+export const sentenceEditorConfig: ISentenceEditorConfig[] = [
   {
     type: commandType.say,
-    title:'普通对话',
+    title: "普通对话",
     initialText: "角色:对话;",
     component: Say
   }
