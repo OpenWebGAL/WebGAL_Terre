581c1cc5f1c1d4b9ec174b493ec6acbff9faf50d
Mahiru <Mahiru_@outlook.com>
2023-02-21 18:23:22 +0800

    feat: (WIP) Say Component
    Former-commit-id: 37aeae33336fed2ad9f8ecfb1649c71e1bc1e5c1

--- packages/origine2/src/pages/editor/GraphicalEditor/SentenceEditor/Say.tsx
+++ packages/origine2/src/pages/editor/GraphicalEditor/SentenceEditor/Say.tsx
@@ -1,5 +1,13 @@
+import { TextField } from "@fluentui/react";
 import { ISentenceEditorProps } from "./index";
+import { useValue } from "../../../../hooks/useValue";
 
-export default function Say(props:ISentenceEditorProps){
-  return <div>Say{props.sentence.content}</div>;
+export default function Say(props: ISentenceEditorProps) {
+  const currentValue = useValue(props.sentence.content);
+  return <div><TextField value={currentValue.value}
+    onChange={(ev, newValue) => {
+      currentValue.set(newValue??'');
+    }}
+    onBlur={()=>props.onSubmit(currentValue.value)}
+  /></div>;
 }
