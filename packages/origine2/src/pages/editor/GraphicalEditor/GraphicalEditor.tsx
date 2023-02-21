3fb638e3edde23a4485ecec7c517b50445c646c5
Mahiru <Mahiru_@outlook.com>
2023-02-21 21:25:44 +0800

    feat: init graphical editor.

--- packages/origine2/src/pages/editor/GraphicalEditor/GraphicalEditor.tsx
+++ packages/origine2/src/pages/editor/GraphicalEditor/GraphicalEditor.tsx
@@ -4,10 +4,10 @@ import axios from "axios";
 import { useSelector } from "react-redux";
 import { RootState } from "../../../store/origineStore";
 import { useEffect } from "react";
-import { sentenceEditorConfig } from "./SentenceEditor";
+import { sentenceEditorConfig, sentenceEditorDefault } from "./SentenceEditor";
 import { WsUtil } from "../../../utils/wsUtil";
 import { mergeToString, splitToArray } from "./utils/sceneTextProcessor";
-import Template from "./SentenceEditor/Template";
+import styles from "./graphicalEditor.module.scss";
 
 interface IGraphicalEditorProps {
   targetPath: string;
@@ -21,7 +21,7 @@ export default function GraphicalEditor(props: IGraphicalEditorProps) {
   function updateScene() {
     const url = `/games/${currentEditingGame}/game/scene/${props.targetName}`;
     axios.get(url).then(res => res.data).then((data) => {
-      sceneText.set(data);
+      sceneText.set(data.toString());
     });
   }
 
@@ -29,7 +29,8 @@ export default function GraphicalEditor(props: IGraphicalEditorProps) {
     updateScene();
   }, []);
 
-  function submitSceneAndUpdate(newScene: string, updateIndex: number) {
+  function submitSceneAndUpdate(newScene: string, index: number) {
+    const updateIndex = index + 1;
     sceneText.set(newScene);
     const params = new URLSearchParams();
     params.append("gameName", currentEditingGame);
@@ -61,11 +62,39 @@ export default function GraphicalEditor(props: IGraphicalEditorProps) {
   }
 
   const parsedScene = parseScene(sceneText.value);
-  const sentenceEditorList = parsedScene.sentenceList.map((sentence, index) => {
-    const SentenceEditor = sentenceEditorConfig.find((e) => e.type === sentence.command)?.component ?? Template;
-    return <SentenceEditor key={sentence.commandRaw + sentence.content} sentence={sentence} onSubmit={(newSentence) => {
-      updateSentenceByIndex(newSentence, index);
-    }} />;
+  const sentenceEditorList = parsedScene.sentenceList.map((sentence, i) => {
+    // 实际显示的行数
+    const index = i + 1;
+    const sentenceConfig = sentenceEditorConfig.find((e) => e.type === sentence.command) ?? sentenceEditorDefault;
+    const SentenceEditor = sentenceConfig.component;
+    return <div className={styles.sentenceEditorWrapper} key={sentence.commandRaw + sentence.content}>
+      <div className={styles.lineNumber}>{index}</div>
+      <div className={styles.seArea}>
+        <div className={styles.head}>
+          <div className={styles.title}>
+            {sentenceConfig.title}
+          </div>
+          <div className={styles.optionButton} style={{ margin: "0 0 0 auto" }}>
+            删除本句
+          </div>
+          <div className={styles.optionButton}>
+            上移本句
+          </div>
+          <div className={styles.optionButton}>
+            下移本句
+          </div>
+          <div className={styles.optionButton}>
+            本句前插入句子
+          </div>
+          <div className={styles.optionButton}>
+            本句后插入句子
+          </div>
+        </div>
+        <SentenceEditor sentence={sentence} onSubmit={(newSentence) => {
+          updateSentenceByIndex(newSentence, i);
+        }} />
+      </div>
+    </div>;
   });
-  return <div>{sentenceEditorList}</div>;
+  return <div className={styles.main}>{sentenceEditorList}</div>;
 }
