4e2728e89cc78bcf618b4e2d52188f015adc11e0
Mahiru <Mahiru_@outlook.com>
2023-02-23 22:20:56 +0800

    feat: draggable sentences & delete sentence.
    Former-commit-id: ce97a23c881cd5acc958d1a6308e65a7d7fb50b8

--- packages/origine2/src/pages/editor/GraphicalEditor/GraphicalEditor.tsx
+++ packages/origine2/src/pages/editor/GraphicalEditor/GraphicalEditor.tsx
@@ -4,10 +4,12 @@ import axios from "axios";
 import { useSelector } from "react-redux";
 import { RootState } from "../../../store/origineStore";
 import { useEffect } from "react";
-import { sentenceEditorConfig, sentenceEditorDefault } from "./SentenceEditor";
 import { WsUtil } from "../../../utils/wsUtil";
 import { mergeToString, splitToArray } from "./utils/sceneTextProcessor";
 import styles from "./graphicalEditor.module.scss";
+import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
+import { sentenceEditorConfig, sentenceEditorDefault } from "./SentenceEditor";
+import { Sort } from "@icon-park/react";
 
 interface IGraphicalEditorProps {
   targetPath: string;
@@ -61,40 +63,78 @@ export default function GraphicalEditor(props: IGraphicalEditorProps) {
     submitSceneAndUpdate(mergeToString(arr), index);
   }
 
-  const parsedScene = parseScene(sceneText.value);
-  const sentenceEditorList = parsedScene.sentenceList.map((sentence, i) => {
-    // 实际显示的行数
-    const index = i + 1;
-    const sentenceConfig = sentenceEditorConfig.find((e) => e.type === sentence.command) ?? sentenceEditorDefault;
-    const SentenceEditor = sentenceConfig.component;
-    return <div className={styles.sentenceEditorWrapper} key={sentence.commandRaw + sentence.content}>
-      <div className={styles.lineNumber}>{index}</div>
-      <div className={styles.seArea}>
-        <div className={styles.head}>
-          <div className={styles.title}>
-            {sentenceConfig.title}
-          </div>
-          <div className={styles.optionButton} style={{ margin: "0 0 0 auto" }}>
-            删除本句
-          </div>
-          <div className={styles.optionButton}>
-            上移本句
-          </div>
-          <div className={styles.optionButton}>
-            下移本句
-          </div>
-          <div className={styles.optionButton}>
-            本句前插入句子
-          </div>
-          <div className={styles.optionButton}>
-            本句后插入句子
-          </div>
+  // 重新记录数组顺序
+  const reorder = (startIndex: number, endIndex: number) => {
+    const result = splitToArray(sceneText.value);
+    const [removed] = result.splice(startIndex, 1);
+    result.splice(endIndex, 0, removed);
+    submitSceneAndUpdate(mergeToString(result), endIndex);
+  };
+
+  function onDragEnd(result: any) {
+    if (!result.destination) {
+      return;
+    }
+    reorder(
+      result.source.index,
+      result.destination.index
+    );
+  }
+
+  const parsedScene = (sceneText.value === "" ? { sentenceList: [] } : parseScene(sceneText.value));
+  return <DragDropContext onDragEnd={onDragEnd}>
+    <Droppable droppableId="droppable">
+      {(provided, snapshot) => (
+        // 下面开始书写容器
+        <div className={styles.main}
+          // provided.droppableProps应用的相同元素.
+          {...provided.droppableProps}
+          // 为了使 droppable 能够正常工作必须 绑定到最高可能的DOM节点中provided.innerRef.
+          ref={provided.innerRef}
+        >
+          {parsedScene.sentenceList.map((sentence, i) => {
+            // 实际显示的行数
+            const index = i + 1;
+            const sentenceConfig = sentenceEditorConfig.find((e) => e.type === sentence.command) ?? sentenceEditorDefault;
+            const SentenceEditor = sentenceConfig.component;
+            return <Draggable key={sentence.content + sentence.commandRaw}
+              draggableId={sentence.content + sentence.commandRaw} index={i}>
+              {(provided, snapshot) => (
+                <div className={styles.sentenceEditorWrapper} key={sentence.commandRaw + sentence.content}
+                  ref={provided.innerRef}
+                  {...provided.draggableProps}
+                >
+                  <div className={styles.lineNumber}><span style={{ padding: "0 6px 0 0" }}>{index}</span>
+                    <Sort {...provided.dragHandleProps} style={{ padding: "5px 0 0 0" }} theme="outline" size="22"
+                      fill="rgba(0,0,0,0.5)" />
+                  </div>
+                  <div className={styles.seArea}>
+                    <div className={styles.head}>
+                      <div className={styles.title}>
+                        {sentenceConfig.title}
+                      </div>
+                      <div className={styles.optionButton} style={{ margin: "0 0 0 auto" }}
+                        onClick={() => deleteOneSentence(i)}>
+                        删除本句
+                      </div>
+                      <div className={styles.optionButton}>
+                        本句前插入句子
+                      </div>
+                      <div className={styles.optionButton}>
+                        本句后插入句子
+                      </div>
+                    </div>
+                    <SentenceEditor sentence={sentence} onSubmit={(newSentence) => {
+                      updateSentenceByIndex(newSentence, i);
+                    }} />
+                  </div>
+                </div>
+              )}
+            </Draggable>;
+          })}
+          {provided.placeholder}
         </div>
-        <SentenceEditor sentence={sentence} onSubmit={(newSentence) => {
-          updateSentenceByIndex(newSentence, i);
-        }} />
-      </div>
-    </div>;
-  });
-  return <div className={styles.main}>{sentenceEditorList}</div>;
+      )}
+    </Droppable>
+  </DragDropContext>;
 }
--- packages/origine2/src/pages/editor/GraphicalEditor/SentenceEditor/Say.tsx
+++ packages/origine2/src/pages/editor/GraphicalEditor/SentenceEditor/Say.tsx
@@ -1,13 +1,46 @@
 import { TextField } from "@fluentui/react";
 import { ISentenceEditorProps } from "./index";
 import { useValue } from "../../../../hooks/useValue";
+import { getArgByKey } from "../utils/getArgByKey";
+import styles from "./sentenceEditor.module.scss";
+import ChooseFile from "../../ChooseFile/ChooseFile";
 
 export default function Say(props: ISentenceEditorProps) {
   const currentValue = useValue(props.sentence.content);
-  return <div><TextField value={currentValue.value}
-    onChange={(ev, newValue) => {
-      currentValue.set(newValue??'');
-    }}
-    onBlur={()=>props.onSubmit(currentValue.value)}
-  /></div>;
+  const currentSpeaker = useValue(getArgByKey(props.sentence, "speaker").toString());
+  const currentVocal = useValue(getArgByKey(props.sentence, "vocal").toString());
+  const isNoSpeaker = useValue(false);
+  const submit = () => {
+    props.onSubmit(`${currentSpeaker.value}${isNoSpeaker.value ? "" : ":"}${currentValue.value}${currentVocal.value === "" ? "" : " -" + currentVocal.value};`);
+  };
+  return <div className={styles.sentenceEditorContent}>
+    <div className={styles.editItem}>
+      角色：<TextField value={currentSpeaker.value}
+        onChange={(ev, newValue) => {
+          currentSpeaker.set(newValue ?? "");
+        }}
+        onBlur={submit}
+      />
+    </div>
+    <div className={styles.editItem}>
+      对话：<TextField styles={{
+        root: {
+          width: "600px"
+        }
+      }} value={currentValue.value}
+      onChange={(ev, newValue) => {
+        currentValue.set(newValue ?? "");
+      }}
+      onBlur={submit}
+      />
+    </div>
+    <div className={styles.editItem}>
+      语音：{currentVocal.value}{"\u00a0\u00a0"}
+      <ChooseFile sourceBase="vocal" onChange={(newName) => {
+        currentVocal.set(newName?.name ?? "");
+        submit();
+      }}
+      extName={[".ogg", ".mp3", ".wav"]} />
+    </div>
+  </div>;
 }
