b9700d083a2a915bc794af52696d9a464c9870ae
Mahiru <Mahiru_@outlook.com>
2023-02-25 21:23:46 +0800

    feat: complete sentence config settings.

---
+++ packages/origine2/src/components/terreToggle/TerreToggle.tsx
@@ -0,0 +1,16 @@
+import { Toggle } from "@fluentui/react";
+
+interface ITerreToggle{
+  title:string,
+  onChange:(newValue:boolean)=>void,
+  onText:string,
+  offText:string,
+  isChecked:boolean
+}
+
+export default function TerreToggle(props:ITerreToggle){
+  return <Toggle styles={{ root: { margin: "0 0 0 12px" } }}
+    checked={props.isChecked}
+    label={props.title} inlineLabel onText={props.onText} offText={props.offText}
+    onChange={(event, checked) => props.onChange(checked ?? false)} />;
+}
