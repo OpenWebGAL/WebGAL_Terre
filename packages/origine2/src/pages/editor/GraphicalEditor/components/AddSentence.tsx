199766069d970ecd1083495857986a2712df856a
Mahiru <Mahiru_@outlook.com>
2023-02-25 21:23:46 +0800

    feat: complete sentence config settings.
    Former-commit-id: b9700d083a2a915bc794af52696d9a464c9870ae

--- packages/origine2/src/pages/editor/GraphicalEditor/components/AddSentence.tsx
+++ packages/origine2/src/pages/editor/GraphicalEditor/components/AddSentence.tsx
@@ -3,6 +3,8 @@ import { useValue } from "../../../../hooks/useValue";
 import { useId } from "@fluentui/react-hooks";
 import { Callout } from "@fluentui/react";
 import styles from "../../ChooseFile/chooseFile.module.scss";
+import { Add } from "@icon-park/react";
+import stylesAs from './addSentence.module.scss';
 
 interface IAddSentenceProps {
   titleText: string;
@@ -13,15 +15,19 @@ export default function AddSentence(props: IAddSentenceProps) {
   const isShowCallout = useValue(false);
   const addButtonId = useId("addbutton");
   const addSentenceButtons = sentenceEditorConfig.map(sentenceConfig => {
-    return <div key={sentenceConfig.type} onClick={() => {props.onChoose(sentenceConfig.initialText);isShowCallout.set(false);}}>
+    return <div className={stylesAs.sentenceTypeButton} key={sentenceConfig.type} onClick={() => {props.onChoose(sentenceConfig.initialText);isShowCallout.set(false);}}>
       <div>
+        {sentenceConfig.icon}
+      </div>
+      <div className={stylesAs.title}>
         {sentenceConfig.title}
       </div>
     </div>;
   });
 
   return <>
-    <div id={addButtonId} onClick={() => isShowCallout.set(!isShowCallout.value)}>
+    <div id={addButtonId} className={stylesAs.addSceneButton} onClick={() => isShowCallout.set(!isShowCallout.value)}>
+      <Add style={{padding:'0 4px 0 0'}} theme="outline" size="16" fill="#333"/>
       {props.titleText}
     </div>
     {isShowCallout.value && (
