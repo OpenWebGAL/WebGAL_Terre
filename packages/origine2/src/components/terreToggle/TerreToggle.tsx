a82af8b3d314e9c4cb90328c8a111ea8ce68534c
Mahiru <Mahiru_@outlook.com>
2023-02-28 22:20:37 +0800

    feat: 抽象“编辑组件”，方便配置编辑项

--- packages/origine2/src/components/terreToggle/TerreToggle.tsx
+++ packages/origine2/src/components/terreToggle/TerreToggle.tsx
@@ -9,7 +9,7 @@ interface ITerreToggle{
 }
 
 export default function TerreToggle(props:ITerreToggle){
-  return <Toggle styles={{ root: { margin: "0 0 0 12px" } }}
+  return <Toggle styles={{ root: { margin: "0 0 0 0" } }}
     checked={props.isChecked}
     label={props.title} inlineLabel onText={props.onText} offText={props.offText}
     onChange={(event, checked) => props.onChange(checked ?? false)} />;
--- packages/origine2/src/pages/editor/GraphicalEditor/SentenceEditor/Say.tsx
+++ packages/origine2/src/pages/editor/GraphicalEditor/SentenceEditor/Say.tsx
@@ -1,10 +1,10 @@
-import { TextField } from "@fluentui/react";
 import { ISentenceEditorProps } from "./index";
 import { useValue } from "../../../../hooks/useValue";
 import { getArgByKey } from "../utils/getArgByKey";
 import styles from "./sentenceEditor.module.scss";
 import ChooseFile from "../../ChooseFile/ChooseFile";
 import TerreToggle from "../../../../components/terreToggle/TerreToggle";
+import CommonOptions from "../components/CommonOption";
 
 export default function Say(props: ISentenceEditorProps) {
   const currentValue = useValue(props.sentence.content);
@@ -16,40 +16,46 @@ export default function Say(props: ISentenceEditorProps) {
   };
   return <div className={styles.sentenceEditorContent}>
     <div className={styles.editItem}>
-      角色：<TextField value={currentSpeaker.value}
-        onChange={(ev, newValue) => {
+      <input value={isNoSpeaker.value ? "" :currentSpeaker.value}
+        onChange={(ev) => {
+          const newValue = ev.target.value;
           currentSpeaker.set(newValue ?? "");
         }}
         onBlur={submit}
-      />
-      {`\u00a0提示：留空角色名可以继承先前的角色名`}
-    </div>
-    <div className={styles.editItem}>
-      对话：<TextField key={currentValue.value} styles={{
-        root: {
-          width: "600px"
-        }
-      }} value={currentValue.value}
-      onChange={(ev, newValue) => {
-        currentValue.set(newValue ?? "");
-      }}
-      onBlur={submit}
+        className={styles.sayInput}
+        placeholder={isNoSpeaker.value ? "旁白模式，无角色名" :"角色名，留空以继承上句"}
+        disabled={isNoSpeaker.value}
       />
     </div>
     <div className={styles.editItem}>
-      语音：{currentVocal.value}{"\u00a0\u00a0"}
-      <ChooseFile sourceBase="vocal" onChange={(newName) => {
-        currentVocal.set(newName?.name ?? "");
-        submit();
-      }}
-      extName={[".ogg", ".mp3", ".wav"]} />
+      <textarea value={ currentValue.value}
+        onChange={(ev) => {
+          const newValue = ev.target.value;
+          currentValue.set(newValue ?? "");
+        }}
+        placeholder="对话"
+        onBlur={submit} className={styles.sayArea}
+      />
     </div>
     <div className={styles.editItem}>
-      旁白模式：
-      <TerreToggle title="" onChange={(newValue) => {
-        isNoSpeaker.set(newValue);
-        submit();
-      }} onText="开启，将不显示角色名" offText="关闭，正常显示角色名" isChecked={isNoSpeaker.value} />
+      <CommonOptions key="isNoDialog" title="旁白模式">
+        <TerreToggle title="" onChange={(newValue) => {
+          isNoSpeaker.set(newValue);
+          submit();
+        }} onText="开启，将不显示角色名" offText="关闭，正常显示角色名" isChecked={isNoSpeaker.value} />
+      </CommonOptions>
+      <CommonOptions key="Vocal" title="语音">
+        <>
+          {currentVocal.value !== "" ? `${currentVocal.value}\u00a0\u00a0` : ""}
+          <ChooseFile sourceBase="vocal" onChange={(newName) => {
+            currentVocal.set(newName?.name ?? "");
+            submit();
+          }}
+          extName={[".ogg", ".mp3", ".wav"]} />
+        </>
+
+      </CommonOptions>
+
     </div>
   </div>;
 }
