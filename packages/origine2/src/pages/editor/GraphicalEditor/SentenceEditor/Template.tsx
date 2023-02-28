e5853278f10613d7c4f4cd9c9438169e20ad18a6
Mahiru <Mahiru_@outlook.com>
2023-02-28 22:20:37 +0800

    feat: 抽象“编辑组件”，方便配置编辑项

--- packages/origine2/src/pages/editor/GraphicalEditor/SentenceEditor/Template.tsx
+++ packages/origine2/src/pages/editor/GraphicalEditor/SentenceEditor/Template.tsx
@@ -1,5 +1,13 @@
+import CommonOptions from "../components/CommonOption";
 import { ISentenceEditorProps } from "./index";
+import styles from "./sentenceEditor.module.scss";
 
 export default function Template(props:ISentenceEditorProps){
-  return <div>{props.sentence.content}</div>;
+  return <div className={styles.sentenceEditorContent}>
+    <div className={styles.editItem} >
+      <CommonOptions key="1" title="编辑组件">
+        <div>在这里放置编辑组件</div>
+      </CommonOptions>
+    </div>
+  </div>;
 }
