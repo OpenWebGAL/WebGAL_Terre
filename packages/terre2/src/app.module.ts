9b2b83e4ce67b38ec035e91174b3877abacd7f2e
Mahiru <Mahiru_@outlook.com>
2023-03-02 16:23:43 +0800

    feat: add LSP base

--- packages/terre2/src/app.module.ts
+++ packages/terre2/src/app.module.ts
@@ -4,9 +4,12 @@ import { AppService } from './app.service';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
 import { ManageGameModule } from './Modules/manage-game/manage-game.module';
+import { LspModule } from './Modules/lsp/lsp.module';
 
 @Module({
   imports: [
+    // 代码提示
+    LspModule,
     ManageGameModule,
     // 静态文件服务：游戏与编辑器静态资源文件
     ServeStaticModule.forRoot({
