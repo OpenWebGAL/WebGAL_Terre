ad5e363c086fbc91a87d09afd67052f912d23990
Mahiru <Mahiru_@outlook.com>
2023-03-04 12:19:07 +0800

    add some edit components
    Former-commit-id: 3d999bec9b7fc9db1429bf145cd80d4ec6957806

---
+++ packages/origine2/src/pages/editor/GraphicalEditor/SentenceEditor/ChangeCallScene.tsx
@@ -0,0 +1,36 @@
+import CommonOptions from "../components/CommonOption";
+import { ISentenceEditorProps } from "./index";
+import styles from "./sentenceEditor.module.scss";
+import { commandType } from "webgal-parser/src/interface/sceneInterface";
+import { useValue } from "../../../../hooks/useValue";
+import ChooseFile from "../../ChooseFile/ChooseFile";
+import TerreToggle from "../../../../components/terreToggle/TerreToggle";
+
+export default function ChangeCallScene(props: ISentenceEditorProps) {
+  const isCallScene = useValue(props.sentence.command === commandType.callScene);
+  const fileName = useValue(props.sentence.content);
+  const submit = () => {
+    props.onSubmit(`${isCallScene.value ? "callScene" : "changeScene"}:${fileName.value}`);
+  };
+
+  return <div className={styles.sentenceEditorContent}>
+    <div className={styles.editItem}>
+      <CommonOptions key="1" title="场景文件">
+        <>
+          {fileName.value}{'\u00a0'}
+          <ChooseFile sourceBase="scene" onChange={(file) => {
+            fileName.set(file?.name ?? "");
+            submit();
+          }} extName={[".txt"]} />
+        </>
+      </CommonOptions>
+      <CommonOptions key="1" title="调用/切换场景">
+        <TerreToggle title="" onChange={(newValue) => {
+          isCallScene.set(newValue);
+          submit();
+        }} onText="调用场景，新场景结束后返回父场景"
+        offText="切换场景，新场景直接替换父场景" isChecked={isCallScene.value} />
+      </CommonOptions>
+    </div>
+  </div>;
+}
