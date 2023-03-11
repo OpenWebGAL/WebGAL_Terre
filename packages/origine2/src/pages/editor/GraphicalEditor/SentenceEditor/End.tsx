1f7cd3a2f3c8a296d999b3b4117866f0c61c473e
Mahiru <Mahiru_@outlook.com>
2023-03-11 22:41:36 +0800

    feat: enhance styles

--- packages/origine2/src/pages/editor/GraphicalEditor/SentenceEditor/End.tsx
+++ packages/origine2/src/pages/editor/GraphicalEditor/SentenceEditor/End.tsx
@@ -1,11 +1,9 @@
-import CommonOptions from "../components/CommonOption";
 import { ISentenceEditorProps } from "./index";
 import styles from "./sentenceEditor.module.scss";
+import CommonTips from "../components/CommonTips";
 
-export default function End(props:ISentenceEditorProps){
+export default function End(props: ISentenceEditorProps) {
   return <div className={styles.sentenceEditorContent}>
-    <div>
-      此指令将结束游戏
-    </div>
+    <CommonTips text="此指令将结束游戏" />
   </div>;
 }
