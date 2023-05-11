7e39040c9481b3596b0129192942426eaad90e71
Yomli <yxr080215@163.com>
2023-05-11 12:46:56 +0800

    语言切换菜单， useTrans 添加动态格式化，优化语言自动获取设置， 部分国际化配置
    Former-commit-id: 651e87f48276fe37d676acf5d7394696a45ab905

--- packages/origine2/src/translations/en.ts
+++ packages/origine2/src/translations/en.ts
@@ -1,1 +1,18 @@
-export const en = {};
+export const en = {
+    common: {
+        delete: 'Delete',
+        exit: 'Exit'
+    },
+
+    dashBoard: {
+        msgs: {
+            created: 'Created'
+        },
+        dialogs: {
+            deleteGame: {
+                title: "Delete Game",
+                subtext: `Are you sure you want to delete "{{gameName}}"?`
+            },
+        }
+    }
+};
