7c1a7d217c395f2e4de2751c0ed288b8f3693d39
Yomli <yxr080215@163.com>
2023-05-11 10:13:56 +0800

    vite接口代理
    Former-commit-id: 3228a9843ef67454c4f4da153c90f273d5794cd6

--- packages/origine2/vite.config.ts
+++ packages/origine2/vite.config.ts
@@ -3,5 +3,20 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  server: {
+    proxy: {
+      // 接口地址代理
+      '/api': {
+        target: 'http://localhost:3001', // 接口的域名
+        secure: true, // 如果是https接口，需要配置这个参数
+        changeOrigin: true, // 如果接口跨域，需要进行这个参数配置
+      },
+      '/games': {
+        target: 'http://localhost:3001', // 接口的域名
+        secure: true, // 如果是https接口，需要配置这个参数
+        changeOrigin: true, // 如果接口跨域，需要进行这个参数配置
+      },
+    },
+  },
 })
