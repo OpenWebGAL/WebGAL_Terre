7c0af02e48c3f978cab0c78b6f954ff24c1ba9df
Yomli <yxr080215@163.com>
2023-05-11 22:38:45 +0800

    some translation config.

--- packages/origine2/src/pages/editor/GraphicalEditor/SentenceEditor/ChangeBg.tsx
+++ packages/origine2/src/pages/editor/GraphicalEditor/SentenceEditor/ChangeBg.tsx
@@ -5,8 +5,10 @@ import ChooseFile from "../../ChooseFile/ChooseFile";
 import { useValue } from "../../../../hooks/useValue";
 import { getArgByKey } from "../utils/getArgByKey";
 import TerreToggle from "../../../../components/terreToggle/TerreToggle";
+import useTrans from "@/hooks/useTrans";
 
 export default function ChangeBg(props: ISentenceEditorProps) {
+  const t = useTrans('editor.graphical.sentences.changeBg.');
   const isNoFile = props.sentence.content === "";
   const isGoNext = useValue(!!getArgByKey(props.sentence, "next"));
   const bgFile = useValue(props.sentence.content);
@@ -17,16 +19,16 @@ export default function ChangeBg(props: ISentenceEditorProps) {
 
   return <div className={styles.sentenceEditorContent}>
     <div className={styles.editItem}>
-      <CommonOptions key="isNoDialog" title="关闭背景">
+      <CommonOptions key="isNoDialog" title={t('options.hide.title')}>
         <TerreToggle title="" onChange={(newValue) => {
           if (!newValue) {
-            bgFile.set("选择背景图片");
+            bgFile.set(t('options.hide.choose'));
           } else
             bgFile.set("none");
           submit();
-        }} onText="关闭背景" offText="显示背景" isChecked={isNoFile} />
+        }} onText={t('options.hide.on')} offText={t('options.hide.off')} isChecked={isNoFile} />
       </CommonOptions>
-      {!isNoFile && <CommonOptions key="1" title="背景文件">
+      {!isNoFile && <CommonOptions key="1" title={t('options.file.title')}>
         <>
           {bgFile.value + "\u00a0\u00a0"}
           <ChooseFile sourceBase="background" onChange={(fileDesc) => {
@@ -36,11 +38,11 @@ export default function ChangeBg(props: ISentenceEditorProps) {
           extName={[".png", ".jpg", ".webp"]} />
         </>
       </CommonOptions>}
-      <CommonOptions key="2" title="连续执行">
+      <CommonOptions key="2" title={t('$editor.graphical.sentences.common.options.goNext.title')}>
         <TerreToggle title="" onChange={(newValue) => {
           isGoNext.set(newValue);
           submit();
-        }} onText="本句执行后执行下一句" offText="本句执行后等待" isChecked={isGoNext.value} />
+        }} onText={t('$editor.graphical.sentences.common.options.goNext.on')} offText={t('$editor.graphical.sentences.common.options.goNext.off')} isChecked={isGoNext.value} />
       </CommonOptions>
     </div>
   </div>;
