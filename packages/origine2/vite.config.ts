b9a78e5fb49bbb063509de3d2044613ecf5eb4da
Yomli <yxr080215@163.com>
2023-05-11 11:00:18 +0800

    translation init and config

--- packages/origine2/vite.config.ts
+++ packages/origine2/vite.config.ts
@@ -1,9 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import {resolve} from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': resolve('src'),
+    },
+  },
   server: {
     proxy: {
       // 接口地址代理
