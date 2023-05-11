396506bee68784e509da3e76021508cbcff33062
Yomli <yxr080215@163.com>
2023-05-11 13:24:46 +0800

    编辑器标题栏国际化
    Former-commit-id: dba5aa08c1bf7670f25774e792e9686027bfcb10

--- packages/origine2/src/translations/en.ts
+++ packages/origine2/src/translations/en.ts
@@ -1,10 +1,26 @@
 export const en = {
     common: {
         delete: 'Delete',
-        exit: 'Exit'
+        exit: 'Exit',
+        create: 'Create'
     },
 
     dashBoard: {
+        createNewGame: {
+            button: 'New Game',
+            dialog: {
+                title: 'Create New Game',
+                text: 'Name of new game',
+                defaultName: 'New Game',
+            }
+        },
+        preview: {
+            noneChecked: 'No Game Selected',
+            editGame: 'Edit Game'
+        },
+        titles: {
+            gameList: 'Game List',
+        },
         msgs: {
             created: 'Created'
         },
@@ -13,6 +29,35 @@ export const en = {
                 title: "Delete Game",
                 subtext: `Are you sure you want to delete "{{gameName}}"?`
             },
+        }
+    },
+
+    editor: {
+        topBar: {
+            editing: 'Editing:',
+            editMode: {
+                title: 'Script Editing Mode',
+                onText: 'On, use script editing',
+                offText: 'Off, use graphical editing'
+            },
+            commandBar: {
+                items: {
+                    language: {
+                        text: 'Language'
+                    },
+                    help: {
+                        text: 'Guide'
+                    },
+                    release: {
+                        text: "Export Game",
+                        items: {
+                            web: "Export as Web",
+                            exe: "Export as Windows App",
+                            android: "Export as Android App"
+                        }
+                    }
+                }
+            }
         }
     }
 };
