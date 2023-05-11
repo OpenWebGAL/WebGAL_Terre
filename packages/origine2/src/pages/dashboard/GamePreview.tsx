651e87f48276fe37d676acf5d7394696a45ab905
Yomli <yxr080215@163.com>
2023-05-11 12:46:56 +0800

    语言切换菜单， useTrans 添加动态格式化，优化语言自动获取设置， 部分国际化配置

--- packages/origine2/src/pages/dashboard/GamePreview.tsx
+++ packages/origine2/src/pages/dashboard/GamePreview.tsx
@@ -1,16 +1,18 @@
 import styles from "./gamepreview.module.scss";
 import { useDispatch } from "react-redux";
 import { setDashboardShow, setEditingGame } from "../../store/statusReducer";
+import useTrans from "@/hooks/useTrans";
 
 interface IGamePreviewProps {
   gameName: string;
 }
 
 export function GamePreview(props: IGamePreviewProps) {
+  const t = useTrans('dashBoard.preview.');
   const dispatch = useDispatch();
   if (props.gameName === "") {
     return <div className={styles.noneChecked}>
-      当前没有游戏被选中
+      {t('noneChecked')}
     </div>;
   }
 
@@ -22,7 +24,7 @@ export function GamePreview(props: IGamePreviewProps) {
   return <div className={styles.preview_main}>
     <div className={styles.preview_title}>
       <span className={styles.preview_title_text}>{props.gameName}</span>
-      <span onClick={() => enterEditor(props.gameName)} className={styles.editGameButton}>编辑游戏</span>
+      <span onClick={() => enterEditor(props.gameName)} className={styles.editGameButton}>{t('editGame')}</span>
     </div>
     {/* eslint-disable-next-line react/iframe-missing-sandbox */}
     <iframe id="gamePreviewIframe" frameBorder="0" className={styles.previewWindow} src={`/games/${props.gameName}`} />
