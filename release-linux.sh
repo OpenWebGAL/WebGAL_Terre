0920955e1ecc57534bf2260c6ab28d9261633e33
22 <60903333+nini22P@users.noreply.github.com>
2023-05-17 11:06:45 +0800

    Change android template package name
    Former-commit-id: b25d662442bf3cb8084a3ebf513fbc360c0e2c3b

--- release-linux.sh
+++ release-linux.sh
@@ -40,7 +40,7 @@ cd release/assets/templates/
 git clone https://github.com/nini22P/WebGAL-Android.git
 mv WebGAL-Android WebGAL_Android_Template
 # MainActivity.kt 移动到主文件夹防止误删
-mv WebGAL_Android_Template/app/src/main/java/io/github/nini22p/webgal/MainActivity.kt WebGAL_Android_Template/app/src/main/java/MainActivity.kt
+mv WebGAL_Android_Template/app/src/main/java/com/openwebgal/demo/MainActivity.kt WebGAL_Android_Template/app/src/main/java/MainActivity.kt
 cd ../../../
 
 cd release
@@ -51,9 +51,10 @@ rm -rf public/games/*
 rm -rf public/games/.gitkeep
 rm -rf assets/templates/WebGAL_Template/game/video/*
 rm -rf assets/templates/WebGAL_Template/game/video/.gitkeep
+rm -rf assets/templates/WebGAL_Android_Template/.github
 rm -rf assets/templates/WebGAL_Android_Template/.git
 rm -rf assets/templates/WebGAL_Android_Template/.gitattributes
 rm -rf assets/templates/WebGAL_Android_Template/app/src/main/assets/webgal/.gitkeep
-rm -rf assets/templates/WebGAL_Android_Template/app/src/main/java/io
+rm -rf assets/templates/WebGAL_Android_Template/app/src/main/java/com
 
 echo "WebGAL Origine is now ready to be deployed."
