a4247e63b5d1c79817c3d8ae1de7610af37d66e4
Mahiru <Mahiru_@outlook.com>
2023-06-18 14:03:03 +0800

    fix: sidebar resize problem
    Former-commit-id: 88ddf60470a7a8491aca005834f474c4596a940f

--- packages/origine2/src/pages/editor/EditorSidebar/EditorSideBar.tsx
+++ packages/origine2/src/pages/editor/EditorSidebar/EditorSideBar.tsx
@@ -1,17 +1,17 @@
 import styles from "./editorSideBar.module.scss";
-import { useSelector } from "react-redux";
-import { RootState } from "../../../store/origineStore";
-import { sidebarTag } from "../../../store/statusReducer";
+import {useSelector} from "react-redux";
+import {RootState} from "../../../store/origineStore";
+import {sidebarTag} from "../../../store/statusReducer";
 import GameConfig from "./SidebarTags/GameConfig/GameConfig";
 import Assets from "./SidebarTags/Assets/Assets";
 import Scenes from "./SidebarTags/Scenes/Scenes";
-import React, { useEffect, useRef } from "react";
+import React, {useEffect, useRef} from "react";
 import useTrans from "@/hooks/useTrans";
 import TagTitleWrapper from "@/components/TagTitleWrapper/TagTitleWrapper";
 
-let lastClientX = 0;
 let startX = 0;
 let prevXvalue = 0;
+let isMouseDown = false;
 
 export default function EditorSideBar() {
   const t = useTrans("editor.sideBar.preview.");
@@ -20,50 +20,64 @@ export default function EditorSideBar() {
   useEffect(() => {
     if (ifRef.current) {
       // @ts-ignore
-      ifRef!.current!.contentWindow.console.log = function() {
+      ifRef!.current!.contentWindow.console.log = function () {
       };
     }
   });
 
   useEffect(() => {
     const storeWidth = localStorage.getItem('sidebar-width');
-    if(!storeWidth){
+    if (!storeWidth) {
       const initWidth = window.innerWidth * 0.35;
-      localStorage.setItem('sidebar-width',initWidth.toString());
+      localStorage.setItem('sidebar-width', initWidth.toString());
       document.body.style.setProperty("--sidebar-width", `${initWidth}px`);
-    }else{
+    } else {
       document.body.style.setProperty("--sidebar-width", `${storeWidth}px`);
     }
   }, []);
 
-  const handleDragStart = (event: React.DragEvent<HTMLDivElement>) => {
-    const img = new Image();
-    event.dataTransfer.setDragImage(img,0,0);
+  const handleDragStart = (event: React.MouseEvent<HTMLDivElement>) => {
     startX = event.clientX;
-    lastClientX = event.clientX;
     const prevX = document.body.style.getPropertyValue("--sidebar-width");
     prevXvalue = parseInt(prevX.substring(0, prevX.length - 2), 10);
+    isMouseDown = true;
   };
 
-  const handleDrag = (event: React.DragEvent<HTMLDivElement>) => {
-    const deltaX = event.clientX - (startX);
-    const newValue = prevXvalue + deltaX;
-    document.body.style.setProperty("--sidebar-width", `${newValue}px`);
+  const handleDrag = (event: MouseEvent) => {
+    if (isMouseDown) {
+      const deltaX = event.clientX - (startX);
+      const newValue = prevXvalue + deltaX;
+      document.body.style.setProperty("--sidebar-width", `${newValue}px`);
+    }
+
   };
 
-  const handleDragEnd = (event: React.DragEvent<HTMLDivElement>) => {
-    const deltaX = event.clientX - (startX);
-    const newValue = prevXvalue + deltaX;
-    localStorage.setItem('sidebar-width',newValue.toString());
-    document.body.style.setProperty("--sidebar-width", `${newValue}px`);
+  const handleDragEnd = (event: MouseEvent) => {
+    setTimeout(()=>{
+      const prevX = document.body.style.getPropertyValue("--sidebar-width");
+      prevXvalue = parseInt(prevX.substring(0, prevX.length - 2), 10);
+      localStorage.setItem('sidebar-width', prevXvalue.toString());
+    },10);
+    isMouseDown = false;
   };
+
+  useEffect(() => {
+    window.addEventListener('mousemove', handleDrag);
+    window.addEventListener('mouseup',handleDragEnd);
+    return () => {
+      window.removeEventListener('mousemove', handleDrag);
+      window.removeEventListener('mouseup',handleDragEnd);
+    };
+  }, []);
 
 
   return <>
     {(state.currentSidebarTag !== sidebarTag.none || state.showPreview) && <div className={styles.editor_sidebar}>
-      <div draggable className={styles.divider} onDragStart={handleDragStart}
-        onDrag={handleDrag}
-        onDragEnd={handleDragEnd} />
+      <div className={styles.divider}
+        onMouseDown={handleDragStart}
+        // onMouseUp={handleDragEnd}
+        // onMouseLeave={handleDragEnd}
+      />
       {state.showPreview && <div className={styles.preview_container}>
         <TagTitleWrapper title={t("title")} extra={<>
           <div onClick={() => {
@@ -81,15 +95,15 @@ export default function EditorSideBar() {
           className="tag_title_button">
             {t("previewInNewTab")}
           </div>
-        </>} />
+        </>}/>
         {/* eslint-disable-next-line react/iframe-missing-sandbox */}
         <iframe ref={ifRef} id="gamePreviewIframe" frameBorder="0" className={styles.previewWindow}
-          src={`/games/${state.currentEditingGame}`} />
+          src={`/games/${state.currentEditingGame}`}/>
       </div>}
-      <div style={{ flex: 1, overflow: "auto" }}>
-        {state.currentSidebarTag === sidebarTag.gameconfig && <GameConfig />}
-        {state.currentSidebarTag === sidebarTag.assets && <Assets />}
-        {state.currentSidebarTag === sidebarTag.scenes && <Scenes />}
+      <div style={{flex: 1, overflow: "auto"}}>
+        {state.currentSidebarTag === sidebarTag.gameconfig && <GameConfig/>}
+        {state.currentSidebarTag === sidebarTag.assets && <Assets/>}
+        {state.currentSidebarTag === sidebarTag.scenes && <Scenes/>}
       </div>
 
     </div>
