3f43bdea1f7849b3d8a6a64632db952d2b1c0913
Mahiru <mahiru_@outlook.com>
2023-07-07 21:52:29 +0800

    fix: update icon size in IconWrapper component

--- packages/origine2/src/components/iconWrapper/IconWrapper.tsx
+++ packages/origine2/src/components/iconWrapper/IconWrapper.tsx
@@ -7,17 +7,18 @@ interface IIconWrapperProps {
 
 export default function IconWrapper(props: IIconWrapperProps) {
   const { size = 16 } = props;
+  const { iconSize = 16 } = props;
   return <div style={
     {
       display: "flex",
       alignItems: "center",
       justifyContent: "center",
-      width: size,
-      height: size
+      width: `${size}px`,
+      height: `${size}px`
     }}>
     <img alt="icon" src={props.src} style={{
-      width: props.iconSize,
-      height: props.iconSize
+      width: `${iconSize}px`,
+      height: `${iconSize}px`
     }} />
   </div>;
 }
