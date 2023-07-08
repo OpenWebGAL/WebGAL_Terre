597205b08940737f8a85d75780ffa67f55dbadac
Mahiru <Mahiru_@outlook.com>
2023-07-08 20:39:25 +0800

    update Linux export
    Former-commit-id: 248d9125bfcb35b37a0c752014b321679e956690

--- packages/terre2/src/util/open.ts
+++ packages/terre2/src/util/open.ts
@@ -4,4 +4,5 @@ import * as process from 'process';
 export function _open(target: string) {
   if (process.platform === 'win32') open(target);
   if (process.platform === 'darwin') open(target);
+  if (process.platform === 'linux') open(target);
 }
