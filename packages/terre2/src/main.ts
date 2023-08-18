aed18f7e798e5b4e45e6ae49fb68e00bb9100f4c
Mahiru <Mahiru_@outlook.com>
2023-08-18 20:25:53 +0800

    update version
    Former-commit-id: c0edb6c1b5791b18fed86cbcaf4cdae453d3e603

--- packages/terre2/src/main.ts
+++ packages/terre2/src/main.ts
@@ -9,7 +9,7 @@ async function bootstrap() {
 }
 
 bootstrap().then(() => {
-  console.log(`WebGAL Terre 4.4.3 starting at ${process.cwd()}`);
+  console.log(`WebGAL Terre 4.4.3.2 starting at ${process.cwd()}`);
   if ((process?.env?.NODE_ENV ?? '') !== 'development')
     _open('http://localhost:3001');
 });
