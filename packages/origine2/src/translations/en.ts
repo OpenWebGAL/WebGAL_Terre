ce97d21ab21190592db8d603a8d77ead409d1395
Mahiru <Mahiru_@outlook.com>
2023-08-30 15:36:16 +0800

    Revert "Fixed incomplete, inappropriate, and difficult translations"

--- packages/origine2/src/translations/en.ts
+++ packages/origine2/src/translations/en.ts
@@ -258,13 +258,6 @@ export const en = {
               title: "Effect",
               on: "On",
               off: "Off"
-            },
-            tips: {
-              setEffect: "Tip: Effects only take effect when you switch to a different background or close the previous background and re-add it. If you want to set an effect for an existing background, use a separate Set Effect command",
-            },
-            duration: {
-              title: "Duration",
-              placeholder: "Duration (Milliseconds)"
             }
           }
         },
@@ -313,13 +306,6 @@ export const en = {
               title: "Effect",
               on: "On",
               off: "Off"
-            },
-            tips: {
-              setEffect: "Tip: Effects only take effect when you switch to a different figure or close the previous figure and re-add it. If you want to set an effect for an existing figure, use a separate Set Effect command",
-            },
-            duration: {
-              title: "Duration",
-              placeholder: "Duration (Milliseconds)"
             }
           }
         },
@@ -359,8 +345,7 @@ export const en = {
               placeholder: "Figure ID",
             },
             duration: {
-              title: "Duration",
-              placeholder: "Duration (Milliseconds)"
+              title: "Duration (Milliseconds)",
             }
           }
         },
@@ -571,14 +556,14 @@ export const en = {
           initText: "setTransform: -duration=0;",
           descText: 'Set effect for figure or background',
           transform: {
-            title: "Position",
+            title: "Transform",
             x: " X：",
             y: " Y：",
           },
           scale: {
             title: "Scale",
-            x: " x：",
-            y: " y：",
+            x: " Scale x：",
+            y: " Scale y：",
           },
           effect: {
             title: "Effect",
