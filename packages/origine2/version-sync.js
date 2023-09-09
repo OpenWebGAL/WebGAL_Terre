60271cc3f956e7020ed17915b15e32c9f5ad5b4f
22 <60903333+nini22P@users.noreply.github.com>
2023-09-09 19:40:46 +0800

    Fix version error

--- packages/origine2/version-sync.js
+++ packages/origine2/version-sync.js
@@ -2,14 +2,14 @@ const { readFile, writeFile } = require('node:fs/promises');
 
 const sync = async () => {
 
-  const package = await readFile('./package.json', { encoding: 'utf-8' });
+  // const package = await readFile('./package.json', { encoding: 'utf-8' });
 
-  const version = JSON.parse(package).version;
+  // const version = JSON.parse(package).version;
 
   const info = await readFile('./src/config/info.ts', { encoding: 'utf-8' });
 
   const newInfo = info
-    .replace(/version: '.*'/, `version: '${version}'`)
+    // .replace(/version: '.*'/, `version: '${version}'`)
     .replace(/releaseTime: '.*'/, `releaseTime: '${new Date().toISOString()}'`);
 
   await writeFile('./src/config/info.ts', newInfo, { encoding: 'utf-8' });
