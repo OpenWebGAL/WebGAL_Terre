effdf2ae75455e4910baebbc84ae1292794682fe
22 <60903333+nini22P@users.noreply.github.com>
2023-09-24 10:07:00 +0800

    Improved version display
    Former-commit-id: 0516551f0a92593d51f8e6ca85080a5661d4fce0

--- packages/origine2/src/config/info.ts
+++ packages/origine2/src/config/info.ts
@@ -5,5 +5,5 @@ export interface Info {
 
 export const __INFO: Info = {
   version: '4.4.5',
-  buildTime: '2023-09-23T05:28:26.398Z',
+  buildTime: '2023-09-23T05:28:26.398Z', // 编译时会通过 version-sync.js 自动更新
 };
