eedc59c06a39b2b38b2fc4cb0f447733ac2a814b
22 <60903333+nini22P@users.noreply.github.com>
2023-09-29 21:26:24 +0800

    Improved version check and open file window
    Former-commit-id: 5264b1071924aecafda7305d29b430e5d96ce575

--- packages/origine2/src/hooks/useRelease.ts
+++ packages/origine2/src/hooks/useRelease.ts
@@ -1,7 +1,8 @@
 import { __INFO } from "@/config/info";
+import { logger } from "@/utils/logger";
 import { useState, useEffect } from "react";
 
-export interface latestRelease {
+export interface LatestRelease {
   version: string,
   releaseTime: string,
 }
@@ -10,27 +11,44 @@ export function useRelease() {
 
   const webgalTerreLatestApiUrl = 'https://api.github.com/repos/MakinoharaShoko/WebGAL_Terre/releases/latest';
 
-  const [latestRelease, setLatestRelease] = useState<latestRelease | null>(null);
+  const [latestRelease, setLatestRelease] = useState<LatestRelease | null>(null);
 
-  const getRelease = async () => {
-    const response = await fetch(webgalTerreLatestApiUrl);
-    const releasedata = await response.json();
-    return releasedata;
+  const getRelease = async (): Promise<LatestRelease | null> => {
+    try {
+      const response = await fetch(webgalTerreLatestApiUrl);
+      switch (response.status) {
+      case 200: {
+        const response = await fetch(webgalTerreLatestApiUrl);
+        const releasedata = await response.json();
+        if (releasedata?.tag_name && releasedata?.published_at) {
+          return {
+            version: releasedata.tag_name,
+            releaseTime: releasedata.published_at,
+          };
+        } else {
+          logger.error("发布版本解析失败");
+          return null;
+        };
+      }
+      case 403: {
+        logger.error("GitHub API 访问限制");
+        return null;
+      }
+      };
+      logger.error("无法访问 GitHub API，请检查网络连接");
+      return null;
+    } catch (_) {
+      logger.error("无法访问 GitHub API，请检查网络连接");
+      return null;
+    };
   };
 
   useEffect(() => {
-    try {
-      getRelease().then(
-        releasedata => setLatestRelease(
-          {
-            version: releasedata?.tag_name,
-            releaseTime: releasedata?.published_at,
-          }
-        )
-      );
-    } catch (error) {
-      console.error(error);
-    }
+    getRelease().then(
+      releasedata => setLatestRelease(
+        (releasedata?.version && releasedata?.releaseTime) ? releasedata : null
+      )
+    );
   }, []);
 
   return latestRelease;
