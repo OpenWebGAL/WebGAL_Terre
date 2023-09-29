5264b1071924aecafda7305d29b430e5d96ce575
22 <60903333+nini22P@users.noreply.github.com>
2023-09-29 21:26:24 +0800

    Improved version check and open file window

--- packages/terre2/src/util/open.ts
+++ packages/terre2/src/util/open.ts
@@ -2,7 +2,9 @@ import open = require('open');
 import * as process from 'process';
 
 export function _open(target: string) {
-  if (process.platform === 'win32') open(target);
-  if (process.platform === 'darwin') open(target);
-  if (process.platform === 'linux') open(target);
+  try {
+    if (process.platform === 'win32') open(target);
+    if (process.platform === 'darwin') open(target);
+    if (process.platform === 'linux') open(target);
+  } catch (_) {};
 }
