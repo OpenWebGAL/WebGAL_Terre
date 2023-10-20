ab365204252166f410eaa75fff131801aba42833
Mahiru <Mahiru_@outlook.com>
2023-10-20 23:20:46 +0800

    feat: add openapi

--- packages/terre2/src/main.ts
+++ packages/terre2/src/main.ts
@@ -3,11 +3,19 @@ import { AppModule } from './app.module';
 import * as process from 'process';
 import { _open } from './util/open';
 import { urlencoded, json } from 'express';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(json({ limit: '50mb' }));
   app.use(urlencoded({ extended: true, limit: '50mb' }));
+  const config = new DocumentBuilder()
+    .setTitle('WebGAL Terre API')
+    .setDescription('API Refrence of WebGAL Terre Editor')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(3001);
 }
 
