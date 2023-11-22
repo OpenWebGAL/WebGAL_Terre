e4d0ca7b8be559afbabe079b04f7ff039d7b12d8
Mahiru <Mahiru_@outlook.com>
2023-11-22 22:58:41 +0800

    fix: restore line in editor

--- packages/origine2/src/runtime/WG_ORIGINE_RUNTIME.ts
+++ packages/origine2/src/runtime/WG_ORIGINE_RUNTIME.ts
@@ -10,7 +10,7 @@ class EditorLineHolder{
   private mapSceneUrlToSentence = new Map<string,number>();
   public recordSceneEdittingLine(sceneUrl:string,lineNumber:number){
     this.mapSceneUrlToSentence.set(sceneUrl,lineNumber);
-    console.log(this.mapSceneUrlToSentence);
+    // console.log(this.mapSceneUrlToSentence);
   }
   public getSceneLine(sceneUrl:string){
     return this.mapSceneUrlToSentence.get(sceneUrl) ??0;
