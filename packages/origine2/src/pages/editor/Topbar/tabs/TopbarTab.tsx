b26f788adbef7a436c92003d0930996d604a1bd7
Mahiru <Mahiru_@outlook.com>
2023-12-09 11:51:25 +0800

    feat: topbar auto hide and links

--- packages/origine2/src/pages/editor/Topbar/tabs/TopbarTab.tsx
+++ packages/origine2/src/pages/editor/Topbar/tabs/TopbarTab.tsx
@@ -1,6 +1,20 @@
-import {ReactNode} from "react";
+import React, {ReactNode, useRef} from "react";
 import s from './topbarTab.module.scss';
 
 export default function TopbarTab(props:{children:ReactNode}){
-  return <div className={s.tag} >{props.children}</div>;
+
+  const topbarTag = useRef<HTMLDivElement>(null);
+
+  const handleScroll = (event: React.WheelEvent<HTMLDivElement>) => {
+    const deltaY = event.deltaY;
+    console.log(`滚动距离：${deltaY}px`);
+    const element = topbarTag.current;
+    if(element){
+      const x = element.scrollLeft;
+      const toX = x + deltaY;
+      element.scrollTo(toX,0);
+    }
+  };
+
+  return <div className={s.tab} ref={topbarTag} onWheel={handleScroll}>{props.children}</div>;
 }
