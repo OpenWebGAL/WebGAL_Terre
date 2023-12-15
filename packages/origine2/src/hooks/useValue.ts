07288613e7db3af350467eab9f9205a0fcdf3f6a
Mahiru <Mahiru_@outlook.com>
2023-12-15 23:44:00 +0800

    Update useValue.ts
    Former-commit-id: 9318db410a2af7ab94dc6c5e67e1d1d55aecf3ef

--- packages/origine2/src/hooks/useValue.ts
+++ packages/origine2/src/hooks/useValue.ts
@@ -1,12 +1,18 @@
-import { useState } from "react";
+import {useState} from "react";
 
 export function useValue<T>(initialState: T) {
   const [value, setValue] = useState<T>(initialState);
   return {
-    value,
-    set: function(newValue: T) {
-      this.value = newValue;
+    _value: value,
+    set: function (newValue: T) {
+      this._value = newValue;
       setValue(newValue);
+    },
+    get value() {
+      return this._value;
+    },
+    set value(newValue) {
+      this.set(newValue);
     }
   };
 }
