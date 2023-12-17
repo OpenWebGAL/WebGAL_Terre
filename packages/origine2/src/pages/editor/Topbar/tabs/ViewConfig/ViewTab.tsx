95d56c299be79969c9a8bb5d4e9de6d8c8ebe18f
Mahiru <Mahiru_@outlook.com>
2023-12-17 12:09:48 +0800

    feat: enhance topbar style
    Former-commit-id: b1f5e0903bce9372c7d75ca12401837452ff0747

--- packages/origine2/src/pages/editor/Topbar/tabs/ViewConfig/ViewTab.tsx
+++ packages/origine2/src/pages/editor/Topbar/tabs/ViewConfig/ViewTab.tsx
@@ -1,34 +1,58 @@
 import TopbarTab from "@/pages/editor/Topbar/components/TopbarTab";
 import {TabItem} from "@/pages/editor/Topbar/components/TabItem";
-import TerreToggle from "@/components/terreToggle/TerreToggle";
 import useTrans from "@/hooks/useTrans";
 import {useDispatch, useSelector} from "react-redux";
 import {RootState} from "@/store/origineStore";
 import {setEnableLivePreview, setShowSidebar} from "@/store/userDataReducer";
 import s from './viewTab.module.scss';
-import { TooltipHost } from "@fluentui/react";
+import {FontIcon, TooltipDelay, TooltipHost} from "@fluentui/react";
+import {IconWithTextItem} from "@/pages/editor/Topbar/components/IconWithTextItem";
+import {eventBus} from "@/utils/eventBus";
 
-export function ViewTab(){
+export function ViewTab() {
 
   const tSidebar = useTrans("editor.sideBar.");
   const dispatch = useDispatch();
-  const isEnableLivePreview = useSelector((state:RootState)=>state.userData.isEnableLivePreview);
-  const isShowSidebar = useSelector((state:RootState)=>state.userData.isShowSidebar);
+  const isEnableLivePreview = useSelector((state: RootState) => state.userData.isEnableLivePreview);
+  const isShowSidebar = useSelector((state: RootState) => state.userData.isShowSidebar);
+  const currentEditGame = useSelector((state: RootState) => state.status.editor.currentEditingGame);
 
   return <TopbarTab>
     <TabItem title="侧边栏">
-      <TerreToggle title="显示侧边栏" isChecked={isShowSidebar} onChange={(v) => {
-        dispatch(setShowSidebar(v));
-      }} onText="" offText=""/>
+      <IconWithTextItem onClick={() => {
+        dispatch(setShowSidebar(!isShowSidebar));
+      }} icon={<FontIcon iconName={isShowSidebar ? "SidePanel" : "ClosePaneMirrored"} className={s.iconColor}/>}
+      text={isShowSidebar ? '显示侧边栏' : '隐藏侧边栏'}/>
     </TabItem>
-    <TabItem title={tSidebar('preview.livePreview')}>
+    <TabItem title="侧边栏游戏预览">
+      <IconWithTextItem
+        onClick={() => {
+          eventBus.emit('refGame');
+        }}
+        icon={<FontIcon iconName="Refresh" className={s.iconColor}/>}
+        text="刷新游戏"
+      />
       <TooltipHost
+        delay={TooltipDelay.zero}
         content={<div className={s.previewTips}>
           {tSidebar('preview.notice')}
         </div>}
-      ><TerreToggle title={tSidebar('preview.livePreview')} isChecked={isEnableLivePreview} onChange={(v) => {
-          dispatch(setEnableLivePreview(v));
-        }} onText="" offText=""/></TooltipHost>
+      >
+        <IconWithTextItem
+          onClick={() => {
+            dispatch(setEnableLivePreview(!isEnableLivePreview));
+          }}
+          icon={<FontIcon iconName={isEnableLivePreview ? "Streaming" : "StreamingOff"} className={s.iconColor}/>}
+          text={isEnableLivePreview ? '实时预览' : '实时预览关闭'}
+        />
+      </TooltipHost>
+      <IconWithTextItem
+        onClick={() => {
+          window.open(`/games/${currentEditGame}`, "_blank");
+        }}
+        icon={<FontIcon iconName="OpenInNewWindow" className={s.iconColor}/>}
+        text="新标签页预览"
+      />
     </TabItem>
   </TopbarTab>;
 }
