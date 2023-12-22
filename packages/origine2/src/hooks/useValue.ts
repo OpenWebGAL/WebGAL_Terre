6eba80551bace20510dfaa2cc7b7649e04b437b6
Mahiru <Mahiru_@outlook.com>
2023-12-22 22:23:10 +0800

    feat: memorize asset directory, enhance UI.
    Former-commit-id: 424e4449d3bd00b19f4d89c19fab2ec580a992fd

--- packages/origine2/src/hooks/useValue.ts
+++ packages/origine2/src/hooks/useValue.ts
@@ -1,14 +1,27 @@
 import {useState} from "react";
 
-export function useValue<T>(initialState: T) {
-  const [value, setValue] = useState<T>(initialState);
+const holdMap = new Map();
+
+export function useValue<T>(initialState: T, isHold?: boolean, key?: string) {
+  const holdKey = key ?? '__value_hold_key__';
+  const fromHoldValue = holdMap.get(holdKey);
+  const [value, setValue] = useState<T>(fromHoldValue ?? initialState);
   return {
     _value: value,
     set: function (newValue: T) {
       this._value = newValue;
+      if (isHold) {
+        holdMap.set(holdKey, newValue);
+      }
       setValue(newValue);
     },
     get value() {
+      if (isHold) {
+        const result = holdMap.get(holdKey);
+        if (result) {
+          return result as T;
+        }
+      }
       return this._value;
     },
     set value(newValue) {
--- packages/origine2/src/pages/editor/Topbar/TopbarTabButtonSpecial.tsx
+++ packages/origine2/src/pages/editor/Topbar/TopbarTabButtonSpecial.tsx
@@ -2,6 +2,9 @@ import s from './topbar.module.scss';
 
 export default function TopbarTabButtonSpecial(props:{text:string,isActive:boolean,onClick?:()=>void}){
   return <button onClick={()=>props?.onClick?.()} className={s.topbar_btn_special+' '+(props.isActive?s.topbar_btn_special_active:'')}>
-    {props.text}
+    <div className={s.specialText}>
+      {props.text}
+    </div>
+
   </button>;
 }
