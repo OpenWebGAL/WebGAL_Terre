958cfe2c6dfc18026c42da4db38bd3e73c383784
Mahiru <Mahiru_@outlook.com>
2023-12-23 21:14:57 +0800

    feat: l2d v3 model support

--- packages/origine2/src/config/info.ts
+++ packages/origine2/src/config/info.ts
@@ -5,5 +5,5 @@ export interface Info {
 
 export const __INFO: Info = {
   version: '4.4.7',
-  buildTime: '2023-12-22T14:20:20.337Z', // 编译时会通过 version-sync.js 自动更新
+  buildTime: '2023-12-23T13:07:56.047Z', // 编译时会通过 version-sync.js 自动更新
 };
