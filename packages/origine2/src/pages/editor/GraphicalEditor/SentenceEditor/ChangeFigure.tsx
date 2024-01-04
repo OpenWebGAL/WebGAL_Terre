c444dc51007fd9f1998c5f5b6d5f976ac5d4d15d
Mahiru <Mahiru_@outlook.com>
2024-01-04 20:16:13 +0800

    Update ChangeFigure.tsx

--- packages/origine2/src/pages/editor/GraphicalEditor/SentenceEditor/ChangeFigure.tsx
+++ packages/origine2/src/pages/editor/GraphicalEditor/SentenceEditor/ChangeFigure.tsx
@@ -46,25 +46,25 @@ export default function ChangeFigure(props: ISentenceEditorProps) {
       axios.get(`/games/${gameName}/game/figure/${figureFile.value}`).then(resp => {
         const data = resp.data;
 
-        if(data.motions){
+        if(data?.motions){
           // 处理 motions
           const motions = Object.keys(data.motions);
           setL2dMotionsList(motions.sort((a, b) => a.localeCompare(b)));
         }
 
         // 处理 expressions
-        if (data.expressions) {
+        if (data?.expressions) {
           const expressions: string[] = data.expressions.map((exp: { name: string }) => exp.name);
           setL2dExpressionsList(expressions.sort((a, b) => a.localeCompare(b)));
         }
 
         // 处理 v3 版本的 model
-        if(data['FileReferences']['Motions']){
+        if(data?.['FileReferences']?.['Motions']){
           const motions = Object.keys(data['FileReferences']['Motions']);
           setL2dMotionsList(motions.sort((a, b) => a.localeCompare(b)));
         }
 
-        if(data['FileReferences']['Expressions']){
+        if(data?.['FileReferences']?.['Expressions']){
           const expressions: string[] = data['FileReferences']['Expressions'].map((exp: { Name: string }) => exp.Name);
           setL2dExpressionsList(expressions.sort((a, b) => a.localeCompare(b)));
         }
