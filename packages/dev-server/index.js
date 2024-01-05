0b017ad2a51f6f8a2e3f89c2c7dc1c4577e03eb2
tinyAdapter <tinyadapter@outlook.com>
2024-01-05 17:16:15 +0800

    feat: listening port at env var WEBGAL_PORT
    Former-commit-id: 85412cb9012a9a4e5284b271748c96982c4cb810

--- packages/dev-server/index.js
+++ packages/dev-server/index.js
@@ -1,12 +1,17 @@
 const express = require("express");
 const { createProxyMiddleware } = require("http-proxy-middleware");
+const { env } = require("process")
+
 const app = express();
 app.set("port", "80");
 
-app.all("*", function(req, res, next) {
+app.all("*", function (req, res, next) {
   // 解决跨域问题
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Content-Type,Content-Length, Authorization, Accept,X-Requested-With");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Content-Type,Content-Length, Authorization, Accept,X-Requested-With"
+  );
   res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
   if (req.method === "OPTIONS") {
     res.send(200);
@@ -15,21 +20,32 @@ app.all("*", function(req, res, next) {
   }
 });
 
-app.use(createProxyMiddleware("/api", {
-  target: "http://localhost:3001",// http代理跨域目标接口
-  changeOrigin: true
-}));
+let WEBGAL_PORT = 3000; // default port
+if (env.WEBGAL_PORT) {
+  WEBGAL_PORT = Number.parseInt(env.WEBGAL_PORT);
+}
 
-app.use(createProxyMiddleware("/games", {
-  target: "http://localhost:3001",// http代理跨域目标接口
-  changeOrigin: true,
-}));
+app.use(
+  createProxyMiddleware("/api", {
+    target: `http://localhost:${WEBGAL_PORT + 1}`, // http代理跨域目标接口
+    changeOrigin: true,
+  })
+);
 
-app.use(createProxyMiddleware("/", {
-  target: "http://localhost:3000",// http代理跨域目标接口
-  ws:true,
-  changeOrigin: true
-}));
+app.use(
+  createProxyMiddleware("/games", {
+    target: `http://localhost:${WEBGAL_PORT + 1}`, // http代理跨域目标接口
+    changeOrigin: true,
+  })
+);
+
+app.use(
+  createProxyMiddleware("/", {
+    target: `http://localhost:${WEBGAL_PORT}`, // http代理跨域目标接口
+    ws: true,
+    changeOrigin: true,
+  })
+);
 
 app.listen(app.get("port"), () => {
   console.log(`反向代理已开启，端口：${app.get("port")}`);
