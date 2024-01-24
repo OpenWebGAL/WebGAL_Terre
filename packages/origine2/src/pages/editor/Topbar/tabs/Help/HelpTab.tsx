d9833c76642e85eed100fc291e3c137672e01637
22 <60903333+nini22P@users.noreply.github.com>
2024-01-24 18:24:32 +0800

    feat: migrating to fluent ui v9
    Former-commit-id: bdf5268b878df212656676ba5f237998d3fa5833

--- packages/origine2/src/pages/editor/Topbar/tabs/Help/HelpTab.tsx
+++ packages/origine2/src/pages/editor/Topbar/tabs/Help/HelpTab.tsx
@@ -1,15 +1,20 @@
 import TopbarTab from "@/pages/editor/Topbar/components/TopbarTab";
 import {TabItem} from "@/pages/editor/Topbar/components/TabItem";
 import {IconWithTextItem} from "@/pages/editor/Topbar/components/IconWithTextItem";
-import {FontIcon} from "@fluentui/react";
 import s from './help.module.scss';
 import {useTranslation} from "react-i18next";
+import { Question24Filled, Question24Regular, bundleIcon } from "@fluentui/react-icons";
 
 export function HelpTab() {
   const {t} = useTranslation();
+  const QuestionIcon = bundleIcon(Question24Filled, Question24Regular);
+
   return <TopbarTab>
     <TabItem title={t("帮助")}>
-      <IconWithTextItem onClick={()=>window.open("https://docs.openwebgal.com/", "_blank")} icon={<FontIcon aria-label="Help" iconName="Help" className={s.helpIconColor}/>} text={t("帮助")}/>
+      <IconWithTextItem
+        onClick={()=>window.open("https://docs.openwebgal.com/", "_blank")}
+        icon={<QuestionIcon aria-label="Help" className={s.helpIconColor}/>} text={t("帮助")}
+      />
     </TabItem>
   </TopbarTab>;
 }
