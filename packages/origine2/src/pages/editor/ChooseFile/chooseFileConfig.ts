e1d70314246ef169ce48eb5bac440eb667fd288a
loliko114514 <2359983693@qq.com>
2024-02-18 10:50:54 +0800

    add template extname
    Former-commit-id: 16ff201bd59cd14435455f30a26b8838426db5b6

--- packages/origine2/src/pages/editor/ChooseFile/chooseFileConfig.ts
+++ packages/origine2/src/pages/editor/ChooseFile/chooseFileConfig.ts
@@ -19,6 +19,7 @@ dirNameToExtNameMap.set('animation', ['.json']);
 dirNameToExtNameMap.set('background', ['.jpg', '.png', '.webp']);
 dirNameToExtNameMap.set('bgm', ['.mp3', '.ogg', '.wav']);
 dirNameToExtNameMap.set('figure', ['.png', '.webp', '.json']);
+dirNameToExtNameMap.set('template', ['.scss']);
 dirNameToExtNameMap.set('scene', ['.txt']);
 dirNameToExtNameMap.set('tex', ['.png', '.webp']);
 dirNameToExtNameMap.set('video', ['.mp4']);
