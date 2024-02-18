b855d2e70adecc92d0f3a48b9ae90f9233787b3c
Mahiru <Mahiru_@outlook.com>
2024-02-18 23:19:07 +0800

    refactor: use StreamableFile instead of res.pipe.
    Former-commit-id: bce2c1fb82d3b2ff257c43b8c78c0bb9c7398154

--- packages/terre2/src/Modules/template-preview/template-preview.controller.ts
+++ packages/terre2/src/Modules/template-preview/template-preview.controller.ts
@@ -1,18 +1,16 @@
 import {
-  ConsoleLogger,
   Controller,
   Get,
   NotFoundException,
   Param,
   Req,
-  Res,
+  StreamableFile,
 } from '@nestjs/common';
 import { TemplatePreviewService } from './template-preview.service';
 
 @Controller('template-preview')
 export class TemplatePreviewController {
   constructor(
-    private readonly logger: ConsoleLogger,
     private readonly templatePreviewService: TemplatePreviewService,
   ) {}
 
@@ -20,24 +18,16 @@ export class TemplatePreviewController {
   getTemplateAsset(
     @Param('path') path: string,
     @Param('templateName') templateName: string,
-    @Res() res: Response,
     @Req() req: Request,
   ) {
     const url = req.url;
-
     const templateFilePath = url.split('/template/')?.[1] ?? '';
     const targetPath = `${templateName}/${templateFilePath}`;
     const readResult =
       this.templatePreviewService.getTemplateFileByPath(targetPath);
     if (readResult) {
-      // 如果文件存在，可以直接使用 res.sendFile(filePath) 来发送文件
-      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-      // @ts-ignore
-      readResult.pipe(res);
+      return new StreamableFile(readResult);
     } else {
-      // 如果文件不存在，你可以尝试手动触发 ServeStaticModule 的逻辑，但这在 NestJS 中不是直接支持的行为
-      // 一个可能的解决方案是抛出一个 NotFoundException，让 NestJS 继续寻找下一个匹配的路由/处理器
-      // 注意：这里抛出的异常会被全局异常过滤器捕获，可能不会直接触发 ServeStaticModule
       throw new NotFoundException('The requested file does not exist.');
     }
   }
