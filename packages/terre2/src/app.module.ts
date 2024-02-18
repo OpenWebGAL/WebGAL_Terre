3cc12d0d1f518500701b5e86c489e3c45fc4b6a8
Mahiru <Mahiru_@outlook.com>
2024-02-18 23:08:11 +0800

    feat: preview template api

--- packages/terre2/src/app.module.ts
+++ packages/terre2/src/app.module.ts
@@ -5,12 +5,14 @@ import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
 import { ManageGameModule } from './Modules/manage-game/manage-game.module';
 import { LspModule } from './Modules/lsp/lsp.module';
+import { TemplatePreviewModule } from './Modules/template-preview/template-preview.module';
 
 @Module({
   imports: [
     // 代码提示
     LspModule,
     ManageGameModule,
+    TemplatePreviewModule,
     // 静态文件服务：游戏与编辑器静态资源文件
     ServeStaticModule.forRoot({
       rootPath: join(process.cwd(), 'public'),
@@ -21,8 +23,13 @@ import { LspModule } from './Modules/lsp/lsp.module';
       rootPath: join(process.cwd(), 'assets', 'templates', 'WebGAL_Template'),
       serveRoot: '/games/:gamename/',
     }),
+    // 静态文件服务：引擎模板预览用游戏
+    ServeStaticModule.forRoot({
+      rootPath: join(process.cwd(), 'assets', 'templates', 'WebGAL_Template'),
+      serveRoot: '/template-preview/:template/',
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule { }
+export class AppModule {}
