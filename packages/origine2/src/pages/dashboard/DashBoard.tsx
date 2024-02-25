f15db2213b8f5065d03941d0f95d00020ff1001d
Mahiru <Mahiru_@outlook.com>
2024-02-25 22:49:55 +0800

    feat: enhance dashboard Tablist style

--- packages/origine2/src/pages/dashboard/DashBoard.tsx
+++ packages/origine2/src/pages/dashboard/DashBoard.tsx
@@ -1,28 +1,52 @@
-import { useEffect, useRef } from "react";
-import { useValue } from "../../hooks/useValue";
+import {useEffect, useRef} from "react";
+import {useValue} from "../../hooks/useValue";
 import axios from "axios";
-import { logger } from "../../utils/logger";
-import { Message, TestRefRef } from "../../components/message/Message";
+import {logger} from "../../utils/logger";
+import {Message, TestRefRef} from "../../components/message/Message";
 import styles from "./dashboard.module.scss";
 import Sidebar from "./Sidebar";
 import TemplateSidebar from "./TemplateSidebar";
 import GamePreview from "./GamePreview";
-import { useSelector } from "react-redux";
-import { RootState } from "../../store/origineStore";
+import {useSelector} from "react-redux";
+import {RootState} from "../../store/origineStore";
 import useTrans from "@/hooks/useTrans";
 import useLanguage from "@/hooks/useLanguage";
-import { language } from "@/store/statusReducer";
+import {language} from "@/store/statusReducer";
 import About from "./About";
-import { WebgalParser } from "../editor/GraphicalEditor/parser";
-import { Card, Menu, MenuItem, MenuList, MenuPopover, MenuTrigger, Toolbar, ToolbarButton, TabList, Tab, TabValue, SelectTabData,SelectTabEvent } from "@fluentui/react-components";
-import { LocalLanguage24Filled, LocalLanguage24Regular, Games24Filled, Games24Regular, Album24Filled, Album24Regular, bundleIcon } from "@fluentui/react-icons";
+import {WebgalParser} from "../editor/GraphicalEditor/parser";
+import {
+  Card,
+  Menu,
+  MenuItem,
+  MenuList,
+  MenuPopover,
+  MenuTrigger,
+  Toolbar,
+  ToolbarButton,
+  TabList,
+  Tab,
+  TabValue,
+  SelectTabData,
+  SelectTabEvent
+} from "@fluentui/react-components";
+import {
+  LocalLanguage24Filled,
+  LocalLanguage24Regular,
+  Games24Filled,
+  Games24Regular,
+  Album24Filled,
+  Album24Regular,
+  bundleIcon, Games28Filled, Games28Regular, GamesFilled, GamesRegular, AlbumFilled, AlbumRegular
+} from "@fluentui/react-icons";
 import {useState} from "react";
+import classNames from "classnames";
 
 // 返回的文件信息（单个）
 interface IFileInfo {
   name: string;
   isDir: boolean;
 }
+
 // 游戏信息
 export interface GameInfo {
   dir: string;
@@ -30,7 +54,7 @@ export interface GameInfo {
   cover: string;
 }
 
-export interface TemplateInfo{
+export interface TemplateInfo {
   dir: string;
   title: string;
 }
@@ -43,21 +67,21 @@ export default function DashBoard() {
 
   const LocalLanguageIcon = bundleIcon(LocalLanguage24Filled, LocalLanguage24Regular);
 
-  const GameIcon = bundleIcon(Games24Filled, Games24Regular);
+  const GameIcon = bundleIcon(GamesFilled, GamesRegular);
 
-  const AlbumIcon = bundleIcon(Album24Filled, Album24Regular);
+  const AlbumIcon = bundleIcon(AlbumFilled, AlbumRegular);
 
-  const isDashboardShow:boolean = useSelector((state: RootState) => state.status.dashboard.showDashBoard);
+  const isDashboardShow: boolean = useSelector((state: RootState) => state.status.dashboard.showDashBoard);
 
   const messageRef = useRef<TestRefRef>(null);
 
-  //左侧栏页签
+  // 左侧栏页签
   const selectedValue = useValue<TabValue>("game");
 
   const onTabSelect = (event: SelectTabEvent, data: SelectTabData) => {
     selectedValue.set(data.value);
     refreashDashboard();
-  }
+  };
 
   // const [selectedValue, setSelectedValue] = useState<TabValue>("game");
 
@@ -67,7 +91,7 @@ export default function DashBoard() {
 
   // 当前选中的游戏
   const currentGame = useValue<string | null>(null);
-  
+
   const setCurrentGame = (e: string | null) => currentGame.set(e);
 
   // 游戏列表
@@ -76,26 +100,26 @@ export default function DashBoard() {
   // 模板列表
   const TemplateInfoList = useValue<Array<TemplateInfo>>([]);
 
-    // 当前选中的模板
-    const currentTemplate = useValue<string | null>(null);
-  
-    const setCurrentTemplate = (e: string | null) => currentTemplate.set(e);
+  // 当前选中的模板
+  const currentTemplate = useValue<string | null>(null);
 
-  async function getDirInfo(url:string) {
+  const setCurrentTemplate = (e: string | null) => currentTemplate.set(e);
+
+  async function getDirInfo(url: string) {
     return await axios.get(url).then(r => r.data);
   }
 
-  async function createGame(gameName:string) {
-    const res = await axios.post("/api/manageGame/createGame", { gameName: gameName }).then(r => r.data);
+  async function createGame(gameName: string) {
+    const res = await axios.post("/api/manageGame/createGame", {gameName: gameName}).then(r => r.data);
     logger.info("创建结果：", res);
     messageRef.current!.showMessage(`${gameName} ` + trans('msgs.created'), 2000);
     refreashDashboard();
     setCurrentGame(null);
   }
 
-  async function createTemplate(templateName:string) {
-    console.log("createTeplate:"+templateName);
-    const res = await axios.post("/api/manageGame/createTemplate", { templateName: templateName }).then(r => r.data);
+  async function createTemplate(templateName: string) {
+    console.log("createTeplate:" + templateName);
+    const res = await axios.post("/api/manageGame/createTemplate", {templateName: templateName}).then(r => r.data);
     logger.info("创建结果：", res);
     // messageRef.current!.showMessage(`${templateName} ` + trans('msgs.created'), 2000);
     refreashDashboard();
@@ -103,15 +127,14 @@ export default function DashBoard() {
   }
 
   function refreashDashboard() {
-    if(selectedValue.value === "game")
-    {
+    if (selectedValue.value === "game") {
       getDirInfo("/api/manageGame/gameList").then(response => {
         const gameList = (response as Array<IFileInfo>)
           .filter(e => e.isDir)
           .map(e => e.name);
         logger.info("返回的游戏列表", gameList);
         const getGameInfoList = gameList.map(
-          async (gameName) : Promise<GameInfo> => {
+          async (gameName): Promise<GameInfo> => {
             const gameConfigData = (await axios.get(`/api/manageGame/getGameConfig/${gameName}`)).data;
             const gameConfig = WebgalParser.parseConfig(gameConfigData);
             return {
@@ -123,8 +146,7 @@ export default function DashBoard() {
         Promise.all(getGameInfoList).then(list => gameInfoList.set(list));
       });
     }
-    if(selectedValue.value === "template")
-    {
+    if (selectedValue.value === "template") {
       getDirInfo("/api/manageGame/templateList").then(response => {
         console.log("refreash template");
         const templateList = (response as Array<IFileInfo>)
@@ -132,7 +154,7 @@ export default function DashBoard() {
           .map(e => e.name);
         logger.info("返回的模板列表", templateList);
         const getTemplateInfoList = templateList.map(
-          async (templateName) : Promise<TemplateInfo> => {
+          async (templateName): Promise<TemplateInfo> => {
             const TemplateConfigData = (await axios.get(`/api/manageGame/getTemplateConfig/${templateName}`)).data;
             return {
               dir: templateName,
@@ -155,15 +177,16 @@ export default function DashBoard() {
   };
 
   return <>
-    { isDashboardShow &&
+    {isDashboardShow &&
       <div className={styles.dashboard_container}>
         <div className={styles.topBar}>
-        WebGAL Terre
+          WebGAL Terre
           <Toolbar>
-            <About />
+            <About/>
             <Menu>
               <MenuTrigger>
-                <ToolbarButton aria-label={t('commandBar.items.language.text')} icon={<LocalLanguageIcon />}>{t('commandBar.items.language.text')}</ToolbarButton>
+                <ToolbarButton aria-label={t('commandBar.items.language.text')}
+                  icon={<LocalLanguageIcon/>}>{t('commandBar.items.language.text')}</ToolbarButton>
               </MenuTrigger>
               <MenuPopover>
                 <MenuList>
@@ -176,34 +199,38 @@ export default function DashBoard() {
           </Toolbar>
         </div>
         <div className={styles.container_main}>
-          <TabList selectedValue={selectedValue.value} onTabSelect={onTabSelect} vertical>
-            <Tab id="Game" icon={<GameIcon />} value="game">{t("游戏")}</Tab>
-            <Tab id="Template" icon={<AlbumIcon />} value="template">{t("模板")}</Tab>
-          </TabList>
-          { selectedValue.value==="game" && <div className={styles.dashboard_main}>
-            <Message ref={messageRef} />
+          <div className={styles.tabListContainer}>
+            <TabList selectedValue={selectedValue.value} onTabSelect={onTabSelect} vertical size="large">
+              <Tab className={classNames(styles.tabItem, selectedValue.value === 'game' ? styles.active : '')} id="Game"
+                icon={<GameIcon fontSize={24}/>} value="game">{t("$游戏")}</Tab>
+              <Tab className={classNames(styles.tabItem, selectedValue.value === 'template' ? styles.active : '')}
+                id="Template" icon={<AlbumIcon fontSize={24}/>} value="template">{t("$模板")}</Tab>
+            </TabList>
+          </div>
+          {selectedValue.value === "game" && <div className={styles.dashboard_main}>
+            <Message ref={messageRef}/>
             {
               currentGame.value &&
-                  <GamePreview
-                    currentGame={currentGame.value}
-                    setCurrentGame={setCurrentGame}
-                    gameInfo={gameInfoList.value.find(e => e.dir === currentGame.value)!}
-                  />
+              <GamePreview
+                currentGame={currentGame.value}
+                setCurrentGame={setCurrentGame}
+                gameInfo={gameInfoList.value.find(e => e.dir === currentGame.value)!}
+              />
             }
             <Sidebar
               refreash={refreash}
               createGame={createGame}
               setCurrentGame={setCurrentGame}
               currentSetGame={currentGame.value}
-              gameList={gameInfoList.value} />
+              gameList={gameInfoList.value}/>
           </div>}
-          {selectedValue.value==="template" && <div className={styles.dashboard_main}>
+          {selectedValue.value === "template" && <div className={styles.dashboard_main}>
             <TemplateSidebar
               refreash={refreash}
               createTemplate={createTemplate}
               setCurrentTemplate={setCurrentTemplate}
               currentSetTemplate={currentTemplate.value}
-              templateList={TemplateInfoList.value} />
+              templateList={TemplateInfoList.value}/>
           </div>}
         </div>
       </div>}
