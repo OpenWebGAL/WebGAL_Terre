d746dfbd42bbd43673be3225ec5e3a7740b834b8
Mahiru <Mahiru_@outlook.com>
2024-03-20 21:11:06 +0800

    add terre electron
    Former-commit-id: e71887cfd4c4f71ae5443e4ef74d1b38ee168461

--- packages/origine2/src/config/info.ts
+++ packages/origine2/src/config/info.ts
@@ -5,5 +5,5 @@ export interface Info {
 
 export const __INFO: Info = {
   version: '4.4.12',
-  buildTime: '2024-03-20T11:50:14.822Z', // 编译时会通过 version-sync.js 自动更新
+  buildTime: '2024-03-20T13:05:21.572Z', // 编译时会通过 version-sync.js 自动更新
 };
