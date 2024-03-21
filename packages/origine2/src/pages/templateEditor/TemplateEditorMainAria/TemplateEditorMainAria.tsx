794ab4c3c3233195533c71c0c1039bb2dbf3dfeb
22 <60903333+nini22P@users.noreply.github.com>
2024-03-21 15:15:51 +0800

    Resizing support

--- packages/origine2/src/pages/templateEditor/TemplateEditorMainAria/TemplateEditorMainAria.tsx
+++ packages/origine2/src/pages/templateEditor/TemplateEditorMainAria/TemplateEditorMainAria.tsx
@@ -1,10 +1,55 @@
 /* eslint-disable react/iframe-missing-sandbox */
+import React, { useEffect, useState } from 'react';
 import useEditorStore from '@/store/useEditorStore';
 import styles from './templateEditorMainAria.module.scss';
+import { useTemplateEditorContext } from '@/store/useTemplateEditorStore';
 
 export default function TemplateMainAria() {
   const templateName = useEditorStore.use.subPage();
-  const editorHeight = 380;
+  const editorHeight = useTemplateEditorContext((state) => state.editorHeight);
+  const updateEditorHeight = useTemplateEditorContext((state) => state.updateEditorHeight);
+
+  const minHeight = 0;
+  const [isDragging, setIsDragging] = useState(false);
+  const [initY, setInitY] = useState(0);
+
+  const handleMouseDown = (event: React.MouseEvent<HTMLDivElement>) => {
+    setIsDragging(true);
+    setInitY(event.clientY);
+    const previewFrame = document.getElementById("templatePreviewIframe");
+    if (previewFrame) {
+      previewFrame.style.pointerEvents = 'none';
+    }
+  };
+
+  useEffect(
+    () => {
+      const moveHandler = (event: any) => {
+        if (!isDragging) return;
+        const deltaY = event.clientY - initY;
+        const newHeight = Math.max(minHeight, editorHeight - deltaY);
+        updateEditorHeight(newHeight);
+      };
+
+      const upHandler = () => {
+        setIsDragging(false);
+        const previewFrame = document.getElementById("templatePreviewIframe");
+        if (previewFrame) {
+          previewFrame.style.pointerEvents = 'auto';
+        }
+      };
+
+      document.addEventListener("mousemove", moveHandler);
+      document.addEventListener("mouseup", upHandler);
+
+      return () => {
+        document.removeEventListener("mousemove", moveHandler);
+        document.removeEventListener("mouseup", upHandler);
+      };
+    },
+    [isDragging, initY]
+  );
+
   return (
     <div className={styles.mainAria}>
       <div className={styles.preview}>
@@ -12,10 +57,11 @@ export default function TemplateMainAria() {
           title={templateName}
           frameBorder={0}
           className={styles.previewWindow}
+          id='templatePreviewIframe'
           src={`/template-preview/${templateName}`}
         />
       </div>
-      <div className={styles.divider}><div className={styles.dividerLine}>‖</div></div>
+      <div className={`${styles.divider} ${isDragging ? styles.dividerActive : ''}`} onMouseDown={handleMouseDown}><div className={styles.dividerLine}>‖</div></div>
       <div className={styles.editor} style={{ height: `${editorHeight}px` }}>Editor</div>
     </div>
   );
