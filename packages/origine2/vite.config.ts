4ca0953eeb16a4f0005758b249d9227f923de032
Mahiru <Mahiru_@outlook.com>
2024-03-24 22:24:35 +0800

    Update vite.config.ts

--- packages/origine2/vite.config.ts
+++ packages/origine2/vite.config.ts
@@ -5,7 +5,7 @@ import { env } from 'process';
 
 let WEBGAL_PORT = 3000; // default port
 if (env.WEBGAL_PORT) {
-  WEBGAL_PORT = Number.parseInt(env.WEBGAL_PORT);
+  WEBGAL_PORT = Number.parseInt(env.WEBGAL_PORT, 10);
 }
 
 // https://vitejs.dev/config/
@@ -26,6 +26,11 @@ export default defineConfig({
         changeOrigin: true, // 如果接口跨域，需要进行这个参数配置
       },
       '/games': {
+        target: `http://localhost:${WEBGAL_PORT + 1}`, // 接口的域名
+        secure: true, // 如果是https接口，需要配置这个参数
+        changeOrigin: true, // 如果接口跨域，需要进行这个参数配置
+      },
+      '/templates': {
         target: `http://localhost:${WEBGAL_PORT + 1}`, // 接口的域名
         secure: true, // 如果是https接口，需要配置这个参数
         changeOrigin: true, // 如果接口跨域，需要进行这个参数配置
