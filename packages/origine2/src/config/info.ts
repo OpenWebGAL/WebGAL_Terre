0f18f781f08a4825092cd768013d747157916a77
Mahiru <Mahiru_@outlook.com>
2024-03-27 20:22:23 +0800

    fix: ws port
    Former-commit-id: afa7fce1bf0cc920d833d65fb117d143d9dd5c8c

--- packages/origine2/src/config/info.ts
+++ packages/origine2/src/config/info.ts
@@ -5,5 +5,5 @@ export interface Info {
 
 export const __INFO: Info = {
   version: '4.4.12',
-  buildTime: '2024-03-20T13:51:32.865Z', // 编译时会通过 version-sync.js 自动更新
+  buildTime: '2024-03-27T12:16:41.259Z', // 编译时会通过 version-sync.js 自动更新
 };
--- packages/origine2/src/pages/editor/MainArea/MainArea.tsx
+++ packages/origine2/src/pages/editor/MainArea/MainArea.tsx
@@ -9,8 +9,27 @@ export default function MainArea() {
   // 建立 WS 连接
   useEffect(()=>{
     try {
-      const loc = window.location.hostname;
-      const wsUrl =  window.location.protocol === 'http:'? `ws://${loc}/api/webgalsync`: `wss://${loc}/api/webgalsync`;
+      const loc: string = window.location.hostname;
+      const protocol: string = window.location.protocol;
+      const port: string = window.location.port; // 获取端口号
+
+      // 默认情况下，不需要在URL中明确指定标准HTTP(80)和HTTPS(443)端口
+      let defaultPort = '';
+      if (port && port !== '80' && port !== '443') {
+        // 如果存在非标准端口号，将其包含在URL中
+        defaultPort = `:${port}`;
+      }
+
+      if (protocol !== 'http:' && protocol !== 'https:') {
+        return;
+      }
+
+      // 根据当前协议构建WebSocket URL，并包括端口号（如果有）
+      let wsUrl = `ws://${loc}${defaultPort}/api/webgalsync`;
+      if (protocol === 'https:') {
+        wsUrl = `wss://${loc}${defaultPort}/api/webgalsync`;
+      }
+
       console.log('正在启动socket连接位于：' + wsUrl);
       const socket = new WebSocket(wsUrl);
       socket.onopen = () => {
