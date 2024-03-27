7bd1c17a474812b75e7601696998b27b21450ecf
Mahiru <Mahiru_@outlook.com>
2024-03-27 20:15:40 +0800

    feat: adjust websocket route
    Former-commit-id: 5c574497b918327844b1178f36f37239a6bc8f21

--- packages/terre2/src/app.controller.ts
+++ packages/terre2/src/app.controller.ts
@@ -1,26 +1,11 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
-import * as ws from 'ws';
 import { ApiTags } from '@nestjs/swagger';
 
 @Controller()
 @ApiTags('Test Server')
 export class AppController {
-  constructor(private readonly appService: AppService) {
-    const wsServer = new ws.WebSocketServer({ port: 9999 });
-
-    const connectionList = [];
-
-    wsServer.on('connection', (conn) => {
-      connectionList.push(conn);
-      conn.on('message', (data) => {
-        const str = data.toString();
-        connectionList.forEach((e) => {
-          e.send(str);
-        });
-      });
-    });
-  }
+  constructor(private readonly appService: AppService) {}
 
   @Get('/api/test')
   apiTest(): string {
