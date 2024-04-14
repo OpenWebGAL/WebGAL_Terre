254b0d69680ce353720c8e6c647b217dd1505ccf
Mahiru <Mahiru_@outlook.com>
2024-04-14 20:50:54 +0800

    feat: add state property group, fix: update parser to solve scss parse problem.

--- packages/origine2/src/pages/templateEditor/TemplateGraphicalEditor/withStateEditor/SingleStateEditor.tsx
+++ packages/origine2/src/pages/templateEditor/TemplateGraphicalEditor/withStateEditor/SingleStateEditor.tsx
@@ -3,12 +3,15 @@ import {t} from "@lingui/macro";
 import PropertyEditor from "@/pages/templateEditor/TemplateGraphicalEditor/propertyEditor";
 import s from './singleStateEditor.module.scss';
 
-
-export default function SingleStateEditor(props: { stateName: string, props: IWebgalCssProp[], onSubmit: () => void }) {
-  const stateNameMap = {
+export function getStateNameMap(){
+  return {
     'hover': t`鼠标悬浮样式`,
     'active': t`鼠标按下样式`
   };
+}
+
+export default function SingleStateEditor(props: { stateName: string, props: IWebgalCssProp[], onSubmit: () => void }) {
+  const stateNameMap = getStateNameMap();
 
   // @ts-ignore
   const thisStateName: string = stateNameMap?.[props.stateName] ?? '';
