4deab714701d6cbb629980a8e83207b7fea65548
Mahiru <Mahiru_@outlook.com>
2024-04-20 22:45:48 +0800

    feat: add potion and custom property edit comp.
    Former-commit-id: 9897acfe4149214d7d69e9ccfa16fa5597cd0cb6

--- packages/origine2/src/pages/templateEditor/TemplateGraphicalEditor/WebgalClassEditor/editorTable.ts
+++ packages/origine2/src/pages/templateEditor/TemplateGraphicalEditor/WebgalClassEditor/editorTable.ts
@@ -2,6 +2,7 @@ import React from "react";
 import {t} from "@lingui/macro";
 import WGFontWeight from "@/pages/templateEditor/TemplateGraphicalEditor/WebgalClassEditor/propertyEditor/WGFontWeight";
 import {IWebgalCssProp} from "@/pages/templateEditor/TemplateGraphicalEditor/utils/extractCss";
+import WGPosition from "@/pages/templateEditor/TemplateGraphicalEditor/WebgalClassEditor/propertyEditor/WGPosition";
 
 export interface IPropertyEditorProps {
   prop: IWebgalCssProp,
@@ -20,5 +21,6 @@ interface IWebGALStylePropertyEditItem {
 export function getEditorTable(): IWebGALStylePropertyEditItem[] {
   return [
     {propName: 'font-weight', propLable: t`字重`, editor: WGFontWeight, initialValue: 'normal'},
+    {propName: 'position', propLable: t`定位方式`, editor: WGPosition, initialValue: 'static'},
   ];
 }
