95b41d922959c70f1bd73219f108d42cec37cea4
22 <60903333+nini22P@users.noreply.github.com>
2024-04-21 15:20:29 +0800

    feat: add font-size, letter-spacing, min-width, max-width, min-height, max-height, cursor
    Former-commit-id: 8c83aab9968aa089ce69eded64587d226f91598d

--- packages/origine2/src/pages/templateEditor/TemplateGraphicalEditor/WebgalClassEditor/editorTable.ts
+++ packages/origine2/src/pages/templateEditor/TemplateGraphicalEditor/WebgalClassEditor/editorTable.ts
@@ -6,6 +6,7 @@ import WGPosition from "@/pages/templateEditor/TemplateGraphicalEditor/WebgalCla
 import WGColor from "@/pages/templateEditor/TemplateGraphicalEditor/WebgalClassEditor/propertyEditor/WGColor";
 import WGCommonLengthEditor
   from "@/pages/templateEditor/TemplateGraphicalEditor/WebgalClassEditor/propertyEditor/WGCommonLengthEditor";
+import WGCursor from "./propertyEditor/WGCursor";
 
 export interface IPropertyEditorProps {
   prop: IWebgalCssProp,
@@ -23,14 +24,21 @@ interface IWebGALStylePropertyEditItem {
 
 export function getEditorTable(): IWebGALStylePropertyEditItem[] {
   return [
+    {propName: 'font-size', propLable: t`字体大小`, editor: WGCommonLengthEditor, initialValue: '12px'},
     {propName: 'font-weight', propLable: t`字重`, editor: WGFontWeight, initialValue: 'normal'},
-    {propName: 'position', propLable: t`定位方式`, editor: WGPosition, initialValue: 'static'},
+    {propName: 'letter-spacing', propLable: t`字母间距`, editor: WGCommonLengthEditor, initialValue: '2.5px'},
+    {propName: 'color', propLable: t`颜色`, editor: WGColor, initialValue: '#000000FF'},
     {propName: 'width', propLable: t`宽度`, editor: WGCommonLengthEditor, initialValue: '100px'},
+    {propName: 'min-width', propLable: t`最小宽度`, editor: WGCommonLengthEditor, initialValue: '100px'},
+    {propName: 'max-width', propLable: t`最大宽度`, editor: WGCommonLengthEditor, initialValue: '100px'},
     {propName: 'height', propLable: t`高度`, editor: WGCommonLengthEditor, initialValue: '50px'},
+    {propName: 'min-height', propLable: t`最小高度`, editor: WGCommonLengthEditor, initialValue: '50px'},
+    {propName: 'max-height', propLable: t`最大高度`, editor: WGCommonLengthEditor, initialValue: '50px'},
+    {propName: 'position', propLable: t`定位方式`, editor: WGPosition, initialValue: 'static'},
     {propName: 'left', propLable: t`（定位用）偏移左侧距离`, editor: WGCommonLengthEditor, initialValue: '5px'},
     {propName: 'right', propLable: t`（定位用）偏移右侧距离`, editor: WGCommonLengthEditor, initialValue: '5px'},
     {propName: 'top', propLable: t`（定位用）偏移顶部距离`, editor: WGCommonLengthEditor, initialValue: '5px'},
     {propName: 'bottom', propLable: t`（定位用）偏移底部距离`, editor: WGCommonLengthEditor, initialValue: '5px'},
-    {propName: 'color', propLable: t`颜色`, editor: WGColor, initialValue: '#000000FF'},
+    {propName: 'cursor', propLable: t`鼠标指针`, editor: WGCursor, initialValue: 'pointer'},
   ];
 }
