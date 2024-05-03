76c44aab5a20c499e3bb3499df7f4e1808f5cf7d
Mahiru <Mahiru_@outlook.com>
2024-05-03 21:34:46 +0800

    revert dev-lsp
    Former-commit-id: a1e9363c3be98a68a59cbe1930bb925084792dd6

--- packages/terre2/terre-custom-libs/vscode-ws-jsonrpc-webgal/build/connection.d.ts
+++
@@ -1,8 +0,0 @@
-import { MessageConnection, Logger } from 'vscode-jsonrpc';
-import { IWebSocket } from './socket/socket.js';
-export declare function listen(options: {
-    webSocket: WebSocket;
-    logger?: Logger;
-    onConnection: (connection: MessageConnection) => void;
-}): void;
-export declare function toSocket(webSocket: WebSocket): IWebSocket;
--- packages/terre2/terre-custom-libs/vscode-ws-jsonrpc-webgal/build/server/index.d.ts
+++
@@ -1,2 +0,0 @@
-export * from './connection.js';
-export * from './launch.js';
