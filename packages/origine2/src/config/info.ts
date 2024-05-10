8589baadfea8a78257d4e7c404049cc65e2cd8d0
Mahiru <Mahiru_@outlook.com>
2024-05-10 11:23:04 +0800

    update engine
    Former-commit-id: 761f49372eaf91d53750eb488d7a73f5dd04e70e

--- packages/origine2/src/config/info.ts
+++ packages/origine2/src/config/info.ts
@@ -5,5 +5,5 @@ export interface Info {
 
 export const __INFO: Info = {
   version: '4.5.0',
-  buildTime: '2024-04-04T06:24:48.463Z', // 编译时会通过 version-sync.js 自动更新
+  buildTime: '2024-05-10T02:34:46.185Z', // 编译时会通过 version-sync.js 自动更新
 };
