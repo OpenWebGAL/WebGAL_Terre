8b598b2b9caf329594c47c1783eef98b26db4337
Mahiru <mahiru_@outlook.com>
2024-05-19 18:20:56 +0800

    add translations

--- packages/origine2/src/pages/templateEditor/TemplateEditorSidebar/ComponentTree/ComponentTree.tsx
+++ packages/origine2/src/pages/templateEditor/TemplateEditorSidebar/ComponentTree/ComponentTree.tsx
@@ -1,6 +1,7 @@
 import useEditorStore from '@/store/useEditorStore';
 import ComponentNode from './ComponentNode';
 import styles from './componentTree.module.scss';
+import {t} from "@lingui/macro";
 
 export interface IComponentNode {
   name: string,
@@ -19,32 +20,32 @@ export default function ComponentTree() {
 
   const componentTree: IComponentNode[] = [
     {
-      name: '标题',
+      name: t`标题`,
       path: `${basePath}/UI/Title/title.scss`,
       nodes: [
-        { name: '标题', class: 'Title_main' },
-        { name: '标题按钮列表', class: 'Title_buttonList' },
-        { name: '标题按钮', class: 'Title_button' },
-        { name: '标题按钮文字', class: 'Title_button_text' },
-        { name: '标题备用背景', class: 'Title_backup_background' },
+        { name: t`标题`, class: 'Title_main' },
+        { name: t`标题按钮列表`, class: 'Title_buttonList' },
+        { name: t`标题按钮`, class: 'Title_button' },
+        { name: t`标题按钮文字`, class: 'Title_button_text' },
+        { name: t`标题备用背景`, class: 'Title_backup_background' },
       ],
     },
     {
-      name: '文本框',
+      name: t`文本框`,
       path: `${basePath}/Stage/TextBox/textbox.scss`,
       nodes: [
-        { name: '文本框', class: 'TextBox_main' },
-        { name: '文本框背景', class: 'TextBox_Background' },
-        { name: '外层文本', class: 'outer' },
-        { name: '内层文本', class: 'inner' },
-        { name: '角色名文本框', class: 'TextBox_showName' },
-        { name: '角色名文本框背景', class: 'TextBox_ShowName_Background' },
-        { name: '小头像容器', class: 'miniAvatarContainer' },
-        { name: '小头像图像', class: 'miniAvatarImg' },
-        { name: '角色名容器', class: 'nameContainer' },
-        { name: '角色名外层文本', class: 'outerName' },
-        { name: '角色名内层文本', class: 'innerName' },
-        { name: '文本框文本', class: 'text' },
+        { name: t`文本框`, class: 'TextBox_main' },
+        { name: t`文本框背景`, class: 'TextBox_Background' },
+        { name: t`外层文本`, class: 'outer' },
+        { name: t`内层文本`, class: 'inner' },
+        { name: t`角色名文本框`, class: 'TextBox_showName' },
+        { name: t`角色名文本框背景`, class: 'TextBox_ShowName_Background' },
+        { name: t`小头像容器`, class: 'miniAvatarContainer' },
+        { name: t`小头像图像`, class: 'miniAvatarImg' },
+        { name: t`角色名容器`, class: 'nameContainer' },
+        { name: t`角色名外层文本`, class: 'outerName' },
+        { name: t`角色名内层文本`, class: 'innerName' },
+        { name: t`文本框文本`, class: 'text' },
       ],
     },
   ];
