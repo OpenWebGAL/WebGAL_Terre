4f7b7c7dba4465dab9ecb5194806aa0d94df73d7
Mahiru <mahiru_@outlook.com>
2024-05-19 17:59:49 +0800

    fix: rename problem #264

--- packages/terre2/src/Modules/webgal-fs/webgal-fs.service.ts
+++ packages/terre2/src/Modules/webgal-fs/webgal-fs.service.ts
@@ -102,10 +102,15 @@ export class WebgalFsService {
    */
   async renameFile(path: string, newName: string) {
     // 取出旧文件的路径
-    const oldPath = join(...decodeURI(path).split(/[\/\\]/g));
+    const rawOldPath = decodeURI(path);
+    let oldPath = join(...rawOldPath.split(/[\/\\]/g));
     const pathAsArray = path.split(/[\/\\]/g);
     const newPathAsArray = pathAsArray.slice(0, pathAsArray.length - 1);
-    const newPath = join(...newPathAsArray, decodeURI(newName));
+    let newPath = join(...newPathAsArray, decodeURI(newName));
+    if (rawOldPath.startsWith('/')) {
+      newPath = '/' + newPath;
+      oldPath = '/' + oldPath;
+    }
 
     return await new Promise((resolve) => {
       fs.rename(oldPath, newPath)
