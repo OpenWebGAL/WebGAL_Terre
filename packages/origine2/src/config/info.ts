35f7263da1d4e1ef4ef6b4e2ef03974a66372a37
Mahiru <Mahiru_@outlook.com>
2024-06-30 12:06:08 +0800

    fix: component tree problem

--- packages/origine2/src/config/info.ts
+++ packages/origine2/src/config/info.ts
@@ -5,5 +5,5 @@ export interface Info {
 
 export const __INFO: Info = {
   version: '4.5.3',
-  buildTime: '2024-06-14T15:46:25.585Z', // 编译时会通过 version-sync.js 自动更新
+  buildTime: '2024-06-30T04:04:15.899Z', // 编译时会通过 version-sync.js 自动更新
 };
