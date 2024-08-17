e730bdfe02f3451b4552244dd3fab046f404efa9
Mahiru <Mahiru_@outlook.com>
2024-08-17 10:53:56 +0800

    feat: revert 'revert lsp'

--- packages/terre2/src/Modules/manage-template/manage-template.module.ts
+++ packages/terre2/src/Modules/manage-template/manage-template.module.ts
@@ -2,10 +2,9 @@ import { Module } from '@nestjs/common';
 import { ManageTemplateService } from './manage-template.service';
 import { WebgalFsModule } from '../webgal-fs/webgal-fs.module';
 import { ManageTemplateController } from './manage-template.controller';
-import { LspModule } from '../lsp/lsp.module';
 
 @Module({
-  imports: [WebgalFsModule, LspModule],
+  imports: [WebgalFsModule],
   providers: [ManageTemplateService],
   controllers: [ManageTemplateController],
 })
---
+++ packages/terre2/terre-custom-libs/vscode-ws-jsonrpc-webgal/build/server/index.d.ts
@@ -0,0 +1,2 @@
+export * from './connection.js';
+export * from './launch.js';
