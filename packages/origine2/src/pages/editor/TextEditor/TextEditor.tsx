154f62acae17def91ac06c495c625e61938a2a39
Akirami <66513481+A-kirami@users.noreply.github.com>
2024-11-12 03:06:14 +0800

    feat: add debounce to cursor and content change events

--- packages/origine2/src/pages/editor/TextEditor/TextEditor.tsx
+++ packages/origine2/src/pages/editor/TextEditor/TextEditor.tsx
@@ -13,6 +13,7 @@ import useEditorStore from '@/store/useEditorStore';
 import { useGameEditorContext } from '@/store/useGameEditorStore';
 import { api } from '@/api';
 import {useValue} from "@/hooks/useValue";
+import {debounce} from '@/utils/debounce';
 
 interface ITextEditorProps {
   targetPath: string;
@@ -42,7 +43,7 @@ export default function TextEditor(props: ITextEditorProps) {
     logger.debug('脚本编辑器挂载');
     lspSceneName.value = sceneName;
     editorRef.current = editor;
-    editor.onDidChangeCursorPosition((event) => {
+    editor.onDidChangeCursorPosition(debounce((event:monaco.editor.ICursorPositionChangedEvent) => {
       const lineNumber = event.position.lineNumber;
       const editorValue = editor.getValue();
       const targetValue = editorValue.split('\n')[lineNumber - 1];
@@ -50,8 +51,10 @@ export default function TextEditor(props: ITextEditorProps) {
       if (!isAfterMount) {
         editorLineHolder.recordSceneEdittingLine(props.targetPath, lineNumber);
       }
-      WsUtil.sendSyncCommand(target?.path??'', lineNumber, targetValue);
-    });
+      if (event.reason === monaco.editor.CursorChangeReason.Explicit) {
+        WsUtil.sendSyncCommand(target?.path??'', lineNumber, targetValue);
+      }
+    }, 250));
     editor.updateOptions({ unicodeHighlight: { ambiguousCharacters: false }, wordWrap: isAutoWarp ? 'on' : 'off' });
     isAfterMount = true;
     updateEditData();
@@ -66,7 +69,7 @@ export default function TextEditor(props: ITextEditorProps) {
    * @param {string} value
    * @param {any} ev
    */
-  function handleChange(value: string | undefined, ev: monaco.editor.IModelContentChangedEvent) {
+  const handleChange = debounce((value: string | undefined, ev: monaco.editor.IModelContentChangedEvent) => {
     if(!isEditorReady.value) return;
     logger.debug('编辑器提交更新');
     const lineNumber = ev.changes[0].range.startLineNumber;
@@ -81,7 +84,7 @@ export default function TextEditor(props: ITextEditorProps) {
       const targetValue = currentText.value.split('\n')[lineNumber - 1];
       WsUtil.sendSyncCommand(target?.path??'', lineNumber, targetValue);
     });
-  }
+  }, 250);
 
   function updateEditData() {
     const path = props.targetPath;
