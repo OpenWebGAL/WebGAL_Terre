1d85c2491117a128c6312e3e7f7e7731b84233c3
22 <60903333+nini22P@users.noreply.github.com>
2024-11-17 17:11:34 +0800

    feat: add cleanLog function to sanitize log strings

--- packages/terre2/src/logger.ts
+++ packages/terre2/src/logger.ts
@@ -1,13 +1,17 @@
 import * as fs from 'fs';
 import * as path from 'path';
 
-const logDir = path.join(process.cwd(), 'logs');
-const logFile = path.join(logDir, 'log.txt');
+const logsDir = path.join(process.cwd(), 'logs');
+const logFile = path.join(logsDir, 'log.txt');
 const logFileMaxSize = 1024 * 1024;
 
 // 确保日志目录存在
-if (!fs.existsSync(logDir)) {
-  fs.mkdirSync(logDir);
+if (!fs.existsSync(logsDir)) {
+  try {
+    fs.mkdirSync(logsDir);
+  } catch (error) {
+    console.error('Error creating logs directory:', error);
+  }
 }
 
 const checkLogFileSize = (filePath: string, maxSize: number) => {
@@ -23,6 +27,12 @@ const checkLogFileSize = (filePath: string, maxSize: number) => {
   });
 };
 
+const cleanLog = (log: string) =>
+  log.replace(
+    /(\x1B\[[0-9;]*m)|(-\s*\d{1,2}\/\d{1,2}\/\d{4},\s*\d{1,2}:\d{2}:\d{2}\s*[AP]M\s+(LOG|ERROR)\s+)/g,
+    '',
+  );
+
 // 在启动时检查日志文件大小
 checkLogFileSize(logFile, logFileMaxSize);
 
@@ -33,14 +43,16 @@ const originalStderrWrite = process.stderr.write;
 
 process.stdout.write = function (...args: any[]) {
   try {
-    logStream.write(`${new Date().toISOString()} [LOG]: ${args[0]}`);
+    logStream.write(`${new Date().toISOString()} [LOG]: ${cleanLog(args[0])}`);
   } catch (_) {}
   return originalStdoutWrite.apply(process.stdout, args);
 };
 
 process.stderr.write = function (...args: any[]) {
   try {
-    logStream.write(`${new Date().toISOString()} [ERROR]: ${args[0]}`);
+    logStream.write(
+      `${new Date().toISOString()} [ERROR]: ${cleanLog(args[0])}`,
+    );
   } catch (_) {}
   return originalStderrWrite.apply(process.stderr, args);
 };
