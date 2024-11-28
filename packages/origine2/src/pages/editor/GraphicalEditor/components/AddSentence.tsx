7610e7daa3d527b8eabdcd2409889aa3b430bc2f
Lingxi <2863878907@qq.com>
2024-11-28 23:06:04 +0800

    feat: AddSentence ShortCut And Ref

--- packages/origine2/src/pages/editor/GraphicalEditor/components/AddSentence.tsx
+++ packages/origine2/src/pages/editor/GraphicalEditor/components/AddSentence.tsx
@@ -1,15 +1,39 @@
-import { sentenceEditorConfig } from "../SentenceEditor";
-import { useValue } from "../../../../hooks/useValue";
-import { Button, Dialog, DialogBody, DialogContent, DialogSurface, DialogTitle, DialogTrigger } from "@fluentui/react-components";
-import { Add } from "@icon-park/react";
+import {sentenceEditorConfig} from "../SentenceEditor";
+import {useValue} from "../../../../hooks/useValue";
+import {
+  Button,
+  Dialog,
+  DialogBody,
+  DialogContent,
+  DialogSurface,
+  DialogTitle,
+  DialogTrigger
+} from "@fluentui/react-components";
+import {Add} from "@icon-park/react";
 import stylesAs from "./addSentence.module.scss";
 import stylesGe from '../graphicalEditor.module.scss';
-import { commandType } from "webgal-parser/src/interface/sceneInterface";
-import { Dismiss24Filled, Dismiss24Regular, bundleIcon } from "@fluentui/react-icons";
+import {commandType} from "webgal-parser/src/interface/sceneInterface";
+import {bundleIcon, Dismiss24Filled, Dismiss24Regular} from "@fluentui/react-icons";
+import React, {forwardRef, useImperativeHandle} from "react";
+
+export const AddSentenceShortCutsConfig: {type: commandType, key: string}[] = [
+  {
+    "type": commandType.say,
+    "key": "S",
+  },
+  {
+    "type": commandType.changeFigure,
+    "key": "F",
+  }
+];
 
 export enum addSentenceType {
   forward,
   backward
+}
+
+export interface AddSentenceMethods {
+  showUp: (title?: string) => void;
 }
 
 interface IAddSentenceProps {
@@ -18,9 +42,19 @@ interface IAddSentenceProps {
   onChoose: (newSentence: string) => void;
 }
 
-export default function AddSentence(props: IAddSentenceProps) {
+export const AddSentence = forwardRef<AddSentenceMethods, IAddSentenceProps>((props: IAddSentenceProps, ref) => {
+  const AddSentenceShortCutsHandle = (e: React.KeyboardEvent<HTMLDivElement>) => {
+    const type = AddSentenceShortCutsConfig.find(config => e.key.toLowerCase() === config.key.toLowerCase())?.type;
+
+    if (type !== undefined) {
+      const initialText = sentenceEditorConfig.find((config) => config.type === type)?.initialText();
+      if (initialText) props.onChoose(initialText);
+      isShowCallout.set(false);
+    }
+  };
   const DismissIcon = bundleIcon(Dismiss24Filled, Dismiss24Regular);
   const isShowCallout = useValue(false);
+  const propsTitle = useValue(props.titleText);
   const addSentenceButtons = sentenceEditorConfig.filter(e => e.type !== commandType.comment).map(sentenceConfig => {
     return <div className={stylesAs.sentenceTypeButton} key={sentenceConfig.type} onClick={() => {
       props.onChoose(sentenceConfig.initialText());
@@ -40,16 +74,23 @@ export default function AddSentence(props: IAddSentenceProps) {
     </div>;
   });
 
+  useImperativeHandle(ref, () => ({
+    showUp: (title?: string) => {
+      isShowCallout.set(!isShowCallout.value);
+      if (title) propsTitle.set(title);
+    },
+  }));
+
   return <>
     <div className={stylesGe.optionButton} onClick={() => isShowCallout.set(!isShowCallout.value)}>
       <Add strokeWidth={3} style={{ padding: "2px 4px 0 0" }} theme="outline" size="16"/>
-      {props.titleText}
+      {propsTitle.value}
     </div>
     <Dialog
       open={isShowCallout.value}
       onOpenChange={() => isShowCallout.set(false)}
     >
-      <DialogSurface style={{ maxWidth: "960px"}}>
+      <DialogSurface style={{ maxWidth: "960px"}} onKeyDown={AddSentenceShortCutsHandle}>
         <DialogBody>
           <DialogTitle
             action={
@@ -61,7 +102,7 @@ export default function AddSentence(props: IAddSentenceProps) {
                 />
               </DialogTrigger>
             }
-          >{props.titleText}</DialogTitle>
+          >{propsTitle.value}</DialogTitle>
           <DialogContent>
             <div className={stylesAs.sentenceTypeButtonList}>
               {addSentenceButtons}
@@ -71,4 +112,4 @@ export default function AddSentence(props: IAddSentenceProps) {
       </DialogSurface>
     </Dialog>
   </>;
-}
+});
