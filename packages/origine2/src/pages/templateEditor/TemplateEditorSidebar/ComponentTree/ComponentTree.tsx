5a7affa7e7339fb4284c7c01eee2722dc8d4bb42
Lingxi <2863878907@qq.com>
2024-12-06 22:16:57 +0800

    feat: i18nfix and ws setComponentVisibility tool

--- packages/origine2/src/pages/templateEditor/TemplateEditorSidebar/ComponentTree/ComponentTree.tsx
+++ packages/origine2/src/pages/templateEditor/TemplateEditorSidebar/ComponentTree/ComponentTree.tsx
@@ -15,59 +15,74 @@ export interface IClassNode {
   class: string,
 }
 
-export const ComponentTreeTextbox = {
-  name: t`文本框`,
-  path: `Stage/TextBox/textbox.scss`,
-  nodes: [
-    { name: t`文本框`, class: 'TextBox_main' },
-    { name: t`文本框（小头像关闭时）`, class: 'TextBox_main_miniavatarOff' },
-    { name: t`文本框背景`, class: 'TextBox_Background' },
-    { name: t`外层文本`, class: 'outer' },
-    { name: t`内层文本`, class: 'inner' },
-    { name: t`角色名文本框`, class: 'TextBox_showName' },
-    { name: t`角色名文本框背景`, class: 'TextBox_ShowName_Background' },
-    { name: t`小头像容器`, class: 'miniAvatarContainer' },
-    { name: t`小头像图像`, class: 'miniAvatarImg' },
-    { name: t`角色名容器`, class: 'nameContainer' },
-    { name: t`角色名外层文本`, class: 'outerName' },
-    { name: t`角色名内层文本`, class: 'innerName' },
-    { name: t`文本框文本`, class: 'text' },
-  ],
+export const TemplateGraphicComponentTreeTitle = () => {
+  return {
+    name: t`标题`,
+    path: `UI/Title/title.scss`,
+    nodes: [
+      { name: t`标题`, class: 'Title_main' },
+      { name: t`标题按钮列表`, class: 'Title_buttonList' },
+      { name: t`标题按钮`, class: 'Title_button' },
+      { name: t`标题按钮文字`, class: 'Title_button_text' },
+      { name: t`标题备用背景`, class: 'Title_backup_background' },
+    ],
+  };
 };
 
-export const ComponentTreeChoose = {
-  name: t`选项`,
-  path: `Stage/Choose/choose.scss`,
-  nodes: [
-    {name: t`选项列表`, class: 'Choose_Main'},
-    {name: t`选项按钮`, class: 'Choose_item'},
-    {name: t`选项按钮（禁用）`, class: 'Choose_item_disabled'},
-    {name: t`选项按钮外层`, class: 'Choose_item_outer'},
-  ],
+export const TemplateGraphicComponentTreeTextbox = () => {
+  return {
+    name: t`文本框`,
+    path: `Stage/TextBox/textbox.scss`,
+    nodes: [
+      { name: t`文本框`, class: 'TextBox_main' },
+      // string 'miniavatarOff' is used by tabsSyncAction function in TemplateEditor.tsx
+      { name: t`文本框（小头像关闭时）`, class: 'TextBox_main_miniavatarOff' },
+      { name: t`文本框背景`, class: 'TextBox_Background' },
+      { name: t`外层文本`, class: 'outer' },
+      { name: t`内层文本`, class: 'inner' },
+      { name: t`角色名文本框`, class: 'TextBox_showName' },
+      { name: t`角色名文本框背景`, class: 'TextBox_ShowName_Background' },
+      { name: t`小头像容器`, class: 'miniAvatarContainer' },
+      { name: t`小头像图像`, class: 'miniAvatarImg' },
+      { name: t`角色名容器`, class: 'nameContainer' },
+      { name: t`角色名外层文本`, class: 'outerName' },
+      { name: t`角色名内层文本`, class: 'innerName' },
+      { name: t`文本框文本`, class: 'text' },
+    ],
+  };
 };
 
-export const ComponentTreeTitle = {
-  name: t`标题`,
-  path: `UI/Title/title.scss`,
-  nodes: [
-    { name: t`标题`, class: 'Title_main' },
-    { name: t`标题按钮列表`, class: 'Title_buttonList' },
-    { name: t`标题按钮`, class: 'Title_button' },
-    { name: t`标题按钮文字`, class: 'Title_button_text' },
-    { name: t`标题备用背景`, class: 'Title_backup_background' },
-  ],
+export const TemplateTempScene = () => {
+  // useLingui(); 不清楚是否需要加这个
+  return {
+    textbox: `WebGal:${t`对话框文字`} -fontSize=default;`,
+    choose: `choose:${t`可选项`}:|${t`不可选项`}:;`
+  };
+};
+
+export const TemplateGraphicComponentTreeChoose = () => {
+  return {
+    name: t`选项`,
+    path: `Stage/Choose/choose.scss`,
+    nodes: [
+      {name: t`选项列表`, class: 'Choose_Main'},
+      {name: t`选项按钮`, class: 'Choose_item'},
+      {name: t`选项按钮（禁用）`, class: 'Choose_item_disabled'},
+      {name: t`选项按钮外层`, class: 'Choose_item_outer'},
+    ],
+  };
 };
 
 export default function ComponentTree() {
   /**
    * Component Tree
-   * 不知道为啥，不写到这打包会有问题
+   * 通过函数形式延迟加载字符，以便适应useLingui();
    */
 
   const componentTree: IComponentNode[] = [
-    ComponentTreeTitle,
-    ComponentTreeTextbox,
-    ComponentTreeChoose
+    TemplateGraphicComponentTreeTitle(),
+    TemplateGraphicComponentTreeTextbox(),
+    TemplateGraphicComponentTreeChoose(),
   ];
 
   const templateName = useEditorStore.use.subPage();
