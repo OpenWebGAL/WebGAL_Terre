29cf18426e0fb1ae12d74590c9a60be54cef6b2c
Mahiru <Mahiru_@outlook.com>
2024-12-14 11:38:11 +0800

    rename function

--- packages/origine2/src/pages/templateEditor/TemplateEditor.tsx
+++ packages/origine2/src/pages/templateEditor/TemplateEditor.tsx
@@ -33,10 +33,10 @@ export const sendComponentPreviewMessage = (componentPath: string, componentClas
   }
   else if (componentPath.includes(useComponentTreeTextbox().path)) {
     const miniAvatar = !componentClass.toLowerCase().includes("miniavataroff") ? "miniavatar.png" : "";
-    WsUtil.createTempScene(`changeBg:bg.png -next;\nminiAvatar:${miniAvatar} -next;\n${useTemplateTempScene().textbox}`);
+    WsUtil.runTempScene(`changeBg:bg.png -next;\nminiAvatar:${miniAvatar} -next;\n${useTemplateTempScene().textbox}`);
   }
   else if (componentPath.includes(useComponentTreeChoose().path)) {
-    WsUtil.createTempScene(`changeBg:bg.png -next;\n${useTemplateTempScene().choose}`);
+    WsUtil.runTempScene(`changeBg:bg.png -next;\n${useTemplateTempScene().choose}`);
   }
 };
 
