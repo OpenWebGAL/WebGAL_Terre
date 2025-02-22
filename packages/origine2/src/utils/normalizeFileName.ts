dbf5be3fa2aedf7f7be6b446ecf70baa5dec7338
22 <60903333+nini22P@users.noreply.github.com>
2025-02-22 19:47:58 +0800

    replace underscores with hyphens in normalizeFileName function

--- packages/origine2/src/utils/normalizeFileName.ts
+++ packages/origine2/src/utils/normalizeFileName.ts
@@ -1,5 +1,5 @@
 const normalizeFileName = (filename: string): string => {
-  const normalized = filename.replace(/[<>《》:：“”'"?？!！/\\|@#%&\s]+/g, '_').trim();
+  const normalized = filename.replace(/[<>《》:：“”'"?？!！/\\|@#%&\s]+/g, '-').trim();
   return normalized.replace(/^_+|_+$/g, '');
 };
 
