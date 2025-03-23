95fc67ec8c30a5492162748fa70113c29fa0ab54
22 <60903333+nini22P@users.noreply.github.com>
2025-03-23 09:38:30 +0800

    update release-android.sh

--- release-android.sh
+++ release-android.sh
@@ -14,8 +14,7 @@ cd dist
 cp -r main.js  ../../../release
 rm main.js
 cd ../
-mkdir Exported_Games
-cp -r public assets Exported_Games ../../release
+cp -r public assets ../../release
 cd ../../
 
 # 进入 Origine 目录
@@ -24,21 +23,33 @@ yarn run build
 cp -rf dist/* ../../release/public/
 cd ../../
 
+# 克隆 WebGAL Android 模板
+cd release/assets/templates/
+git clone https://github.com/nini22P/WebGAL-Android.git
+mv WebGAL-Android WebGAL_Android_Template
+# MainActivity.kt 移动到主文件夹防止误删
+mv WebGAL_Android_Template/app/src/main/java/com/openwebgal/demo/MainActivity.kt WebGAL_Android_Template/app/src/main/java/MainActivity.kt
+cd ../../../
+
 cd release
 
 # 删除冗余文件
-rm -rf Exported_Games/*
 rm -rf public/games/*
 rm -rf public/games/.gitkeep
 rm -rf assets/templates/WebGAL_Template/game/video/*
 rm -rf assets/templates/WebGAL_Template/game/video/.gitkeep
+rm -rf assets/templates/WebGAL_Android_Template/.github
+rm -rf assets/templates/WebGAL_Android_Template/.git
+rm -rf assets/templates/WebGAL_Android_Template/.gitattributes
+rm -rf assets/templates/WebGAL_Android_Template/app/src/main/assets/webgal/.gitkeep
+rm -rf assets/templates/WebGAL_Android_Template/app/src/main/java/com
 
 cd ../
 
 # 复制文件
 rm -rf packages/terre-android/app/src/main/assets/terre
 mkdir -p packages/terre-android/app/src/main/assets/terre
-cp -rf release/* packages/terre-android/app/src/main/assets/terre
+mv release/* packages/terre-android/app/src/main/assets/terre
 
 cd packages/terre-android
 
