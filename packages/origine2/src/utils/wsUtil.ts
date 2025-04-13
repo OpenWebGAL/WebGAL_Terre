6576104f847e6485cf6702c2d2d53772397e5dd2
Mahiru <Mahiru_@outlook.com>
2025-04-13 10:47:25 +0800

    fix: check ws connection before send sync message.

--- packages/origine2/src/utils/wsUtil.ts
+++ packages/origine2/src/utils/wsUtil.ts
@@ -1,10 +1,64 @@
 import {logger} from "./logger";
 import {DebugCommand, IComponentVisibilityCommand, IDebugMessage} from "@/types/debugProtocol";
 import useEditorStore from "@/store/useEditorStore";
+import {eventBus} from "@/utils/eventBus";
+
+const wsState = {
+  isInit: false
+};
 
 export class WsUtil {
 
-  public static sendMessageToCurrentWs(data: IDebugMessage['data'], event?: IDebugMessage['event']){
+  public static async init(){
+    return new Promise((resolve,reject) => {
+      if(wsState.isInit){
+        resolve(true);
+        return;
+      }
+      try {
+        const loc: string = window.location.hostname;
+        const protocol: string = window.location.protocol;
+        const port: string = window.location.port; // 获取端口号
+
+        // 默认情况下，不需要在URL中明确指定标准HTTP(80)和HTTPS(443)端口
+        let defaultPort = '';
+        if (port && port !== '80' && port !== '443') {
+          // 如果存在非标准端口号，将其包含在URL中
+          defaultPort = `:${port}`;
+        }
+
+        if (protocol !== 'http:' && protocol !== 'https:') {
+          return;
+        }
+
+        // 根据当前协议构建WebSocket URL，并包括端口号（如果有）
+        let wsUrl = `ws://${loc}${defaultPort}/api/webgalsync`;
+        if (protocol === 'https:') {
+          wsUrl = `wss://${loc}${defaultPort}/api/webgalsync`;
+        }
+
+        console.log('正在启动socket连接位于：' + wsUrl);
+        const socket = new WebSocket(wsUrl);
+        socket.onopen = () => {
+          console.log('socket已连接');
+          socket.send('WebGAL Origine 已和 Terre 建立连接');
+          wsState.isInit = true;
+          resolve(true);
+        };
+        socket.onmessage = (e) => {
+          eventBus.emit('get-ws-message', e.data);
+        };
+        // @ts-ignore
+        window['currentWs'] = socket;
+      } catch (e) {
+        console.warn('ws连接失败');
+        reject(e);
+      }
+    });
+  }
+
+  public static async sendMessageToCurrentWs(data: IDebugMessage['data'], event?: IDebugMessage['event']){
+    await this.init();
     // @ts-ignore
     if (window["currentWs"]) {
       logger.debug("编辑器开始发送同步数据");
