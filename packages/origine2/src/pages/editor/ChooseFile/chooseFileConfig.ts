88b23bf16540e2b8cf0d321883ac710fa5a8225b
22 <60903333+nini22P@users.noreply.github.com>
2025-06-08 09:13:13 +0800

    fix: update extNameMap

--- packages/origine2/src/pages/editor/ChooseFile/chooseFileConfig.ts
+++ packages/origine2/src/pages/editor/ChooseFile/chooseFileConfig.ts
@@ -1,10 +1,9 @@
-
 export type IExtNameType = 'scene' | 'image' | 'audio' | 'video' | 'tex' | 'json';
 export const extNameMap = new Map<IExtNameType, string[]>([]);
 
 extNameMap.set('scene', ['.txt']);
 extNameMap.set('image', ['.png', '.jpg', '.jpeg', '.webp']);
-extNameMap.set('video', ['.mp4', '.webm', 'mov', 'avi', 'mkv']);
+extNameMap.set('video', ['.mp4', '.webm', '.mkv']);
 extNameMap.set('audio', ['.mp3', '.ogg', '.wav']);
 extNameMap.set('tex', ['.png', '.webp']);
 extNameMap.set('json', ['.json']);
