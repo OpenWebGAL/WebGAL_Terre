89984dc3afe60bf5b6c1a148bbbe80bbcda54eea
boomwwww <boom_w_w@outlook.com>
2025-08-12 20:57:33 +0800

    Update eventBus.ts

--- packages/origine2/src/utils/eventBus.ts
+++ packages/origine2/src/utils/eventBus.ts
@@ -1,57 +1,21 @@
-import mitt, { type Handler } from 'mitt';
+import mitt from 'mitt';
 
 // eslint-disable-next-line @typescript-eslint/consistent-type-definitions
-type WebSocketEvent = {
+type WebSocketEvents = {
   'web-socket:on-message': { message: string };
 };
 
 // eslint-disable-next-line @typescript-eslint/consistent-type-definitions
-type IframeEvent = {
+type IframeEvents = {
   'iframe:refresh-game': null;
 };
 
 // eslint-disable-next-line @typescript-eslint/consistent-type-definitions
-type EditorEvent = {
+type EditorEvents = {
   'editor:update-scene': { scene: string };
   'editor:topbar-add-sentence': { sentence: string };
 };
 
-type Events = WebSocketEvent & IframeEvent & EditorEvent;
+type Events = WebSocketEvents & IframeEvents & EditorEvents;
 
-const bus = mitt<Events>();
-
-const handlers = new Map<keyof Events, Set<Handler<Events[keyof Events]>>>();
-
-const all = bus.all;
-
-/** 订阅事件  */
-const on = <K extends keyof Events>(type: K, handler: Handler<Events[K]>): void => {
-  const handlerSet = handlers.get(type);
-  if (!handlerSet) {
-    const newHandlerSet = new Set<Handler<Events[keyof Events]>>();
-    newHandlerSet.add(handler as Handler<Events[keyof Events]>);
-    handlers.set(type, newHandlerSet);
-  } else {
-    handlerSet.add(handler as Handler<Events[keyof Events]>);
-  }
-  bus.on(type, handler);
-};
-
-/** 发布事件 */
-const emit = <K extends keyof Events>(type: K, event: Events[K]): void => {
-  bus.emit(type, event);
-};
-
-/** 取消订阅事件 */
-const off = <K extends keyof Events>(type: K, handler: Handler<Events[K]>): void => {
-  const handlerSet = handlers.get(type);
-  if (handlerSet) {
-    handlerSet.delete(handler as Handler<Events[keyof Events]>);
-    if (handlerSet.size === 0) {
-      handlers.delete(type);
-    }
-  }
-  bus.off(type, handler);
-};
-
-export const eventBus = { all, on, emit, off };
+export const eventBus = mitt<Events>();
