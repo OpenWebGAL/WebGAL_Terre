f1b2e74f07b683a693a479ae1a7377155bf66dc8
Mahiru <Mahiru_@outlook.com>
2025-10-19 23:30:29 +0800

    不知为何加上 id 会导致切换主题后代码编辑器颜色模式不生效
    This reverts commit 1a35e484b08afac52b39a4b672757d6ad990f77d.

--- packages/origine2/src/webgalscript/extension.ts
+++ packages/origine2/src/webgalscript/extension.ts
@@ -9,14 +9,15 @@ const manifest: IExtensionManifest = {
   },
   contributes: {
     themes: [
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore id type problem
       {
-        id: 'webgal-white',
         label: 'WebGAL White',
         path: '/white.json',
         uiTheme: 'vs',
       },
+      // @ts-ignore id type problem
       {
-        id: 'webgal-black',
         label: 'WebGAL Black',
         path: '/black.json',
         uiTheme: 'vs',
