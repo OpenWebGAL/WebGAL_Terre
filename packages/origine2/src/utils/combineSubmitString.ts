5a4d7f863ccaee0e2f488a2f86c0371e2811f4c4
Mahiru <Mahiru_@outlook.com>
2025-11-25 21:37:18 +0800

    update eslint rule

--- packages/origine2/src/utils/combineSubmitString.ts
+++ packages/origine2/src/utils/combineSubmitString.ts
@@ -8,6 +8,7 @@ import { arg } from "webgal-parser/src/interface/sceneInterface";
  * @param newArgs 新的参数数组, 若 fullMode 为 false, 会将值为 true 的 arg 简写为 -arg, 省略值为 false 或空字符串的 arg
  * @returns 合并后的用于提交的字符串
  */
+// eslint-disable-next-line max-params
 export function combineSubmitString (
   commandStr: string | undefined,
   content: string,
@@ -33,7 +34,7 @@ export function combineSubmitString (
   unsupportedArg.forEach((v, k, m) => {
     argStrings.push(argToString(k, v));
   });
-    
+
   let combinedString = "";
   if (commandStr !== undefined) {
     combinedString += `${commandStr}:`;
