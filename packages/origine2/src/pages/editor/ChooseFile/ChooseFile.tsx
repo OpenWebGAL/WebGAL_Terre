f94fac59a3e530d6d70d9bb6baf80ec6f54c3f70
Hardy--Lee <hardy--lee@outlook.com>
2026-02-05 20:36:03 +0800

    feat: check invalid file path

--- packages/origine2/src/pages/editor/ChooseFile/ChooseFile.tsx
+++ packages/origine2/src/pages/editor/ChooseFile/ChooseFile.tsx
@@ -7,7 +7,7 @@ import {api} from "@/api";
 import {t} from "@lingui/macro";
 import Assets, {IFile, IFileConfig, IFileFunction} from "@/components/Assets/Assets";
 import {join} from 'path';
-import { ReactNode } from "react";
+import { ReactNode, useCallback } from "react";
 
 export interface IChooseFile {
   title?: string;
@@ -28,17 +28,63 @@ export default function ChooseFile(props: IChooseFile) {
 
   const isShowChooseFileCallout = useValue(false);
 
+  const invalidPath = useValue('');
+  const invalidRange = useValue<{start: number; width: number} | null>(null);
+
+  function checkFilePathValidity(filePath: string): boolean {
+    // 检查是否包含非法字符
+    const argSymbolIndex = filePath.indexOf(' -');
+
+    let isValid = true;
+    if (argSymbolIndex !== -1) {
+      isValid = false;
+      invalidPath.value = filePath;
+      invalidRange.value = {start: argSymbolIndex, width: 2};
+    } else {
+      invalidPath.value = '';
+      invalidRange.value = null;
+    }
+    return isValid;
+  }
+
+  function invalidPathElement() {
+    if (!invalidRange.value) {
+      return null;
+    }
+    const strBeforeInvalid = invalidPath.value.substring(0, invalidRange.value.start);
+    const strInvalid = invalidPath.value.substring(invalidRange.value.start, invalidRange.value.start + invalidRange.value.width);
+    const strAfterInvalid = invalidPath.value.substring(invalidRange.value.start + invalidRange.value.width);
+    return (
+      <div className={styles.chooseFileFooterWarningPath}>
+        {strBeforeInvalid}
+        <div className={styles.chooseFileFooterWarningCode}>
+          {strInvalid}
+        </div>
+        {strAfterInvalid}
+      </div>
+    );
+  }
+
   function toggleIsCalloutVisible() {
+    invalidPath.value = '';
+    invalidRange.value = null;
     isShowChooseFileCallout.set(!isShowChooseFileCallout.value);
   }
 
   async function onChooseFile(file: IFile, type: 'scene' | 'asset') {
-    toggleIsCalloutVisible();
     const prefix = `${basePath.join('/')}/`;
     // 移除开头的 basePath/
-    const fileName = file.path.startsWith(prefix)
+    let fileName = file.path.startsWith(prefix)
       ? file.path.substring(prefix.length)
       : file.path;
+    // 转义;
+    fileName = fileName.replace(';', '\\;');
+
+    if (!checkFilePathValidity(fileName)) {
+      return;
+    }
+  
+    toggleIsCalloutVisible();
     props.onChange({
       ...file,
       name: fileName,
@@ -81,6 +127,12 @@ export default function ChooseFile(props: IChooseFile) {
             fileConfig={fileConfig}
             allowedExtNames={props.extNames}
           />
+          { invalidRange.value &&
+            <div className={styles.chooseFileFooterWarning}>
+              {t`文件路径含有非法字符`}
+              {invalidPathElement()}
+            </div>
+          }
         </div>
       </PopoverSurface>
     </Popover>
