4e9bfb48d67041d1144552c7700c84e9abb210a8
Mahiru <Mahiru_@outlook.com>
2026-03-28 11:22:17 +0800

    template editor support new component class

--- packages/origine2/src/pages/templateEditor/TemplateEditorSidebar/ComponentTree/ComponentTree.tsx
+++ packages/origine2/src/pages/templateEditor/TemplateEditorSidebar/ComponentTree/ComponentTree.tsx
@@ -24,7 +24,11 @@ export const useComponentTreeTitle = () => {
       { name: t`标题按钮列表`, class: 'Title_buttonList' },
       { name: t`标题按钮`, class: 'Title_button' },
       { name: t`标题按钮文字`, class: 'Title_button_text' },
+      { name: t`标题按钮文字占位`, class: 'Title_button_text_placeholder' },
+      { name: t`标题按钮文字外层`, class: 'Title_button_text_outer' },
+      { name: t`标题按钮文字内层`, class: 'Title_button_text_inner' },
       { name: t`标题备用背景`, class: 'Title_backup_background' },
+      { name: t`标题按钮禁用`, class: 'Title_button_disabled' },
     ],
   };
 };
