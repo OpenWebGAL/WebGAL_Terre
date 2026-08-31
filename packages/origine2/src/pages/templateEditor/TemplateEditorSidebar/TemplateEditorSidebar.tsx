89bc166876bfe7b4feeceaf811d230405de73d1b
boomwwww <boom_w_w@outlook.com>
2026-09-01 01:55:58 +0800

    chore(templateEditor): remove unused useRef import

--- packages/origine2/src/pages/templateEditor/TemplateEditorSidebar/TemplateEditorSidebar.tsx
+++ packages/origine2/src/pages/templateEditor/TemplateEditorSidebar/TemplateEditorSidebar.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useMemo, useRef, useState } from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import Assets, { IFileFunction } from '@/components/Assets/Assets';
 import ComponentTree from './ComponentTree/ComponentTree';
 import styles from './templateEditorSidebar.module.scss';
